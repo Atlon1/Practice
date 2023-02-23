e1ceda13d4727c9c997a61b73e95e65848fe7d7f
Atlon1 <maciej.szajstek@gmail.com>
2023-02-23 16:37:56 +0100

    Other task done

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = ['Alex', 'Jacob', 'Mark', 'Max', 'Max']
-
-
+    const arr1 = [1, 2, 3, 12]
 
 
     const result = (a) => {
-     if ( a.length === 0) {
-         return 'no one likes this'
-     } else if (a.length === 1) {
-         return `${a[0]},likes this`
-     } else if (a.length === 2) {
-         return `${a[0]} and ${a[1]},likes this`
-     } else if (a.length === 3) {
-         return `${a[0]},${a[1]} and ${a[2]},likes this`
-     } else {
-         return `${a[0]},${a[1]} and ${a.length - 2} others,likes this`
-     }
+
+        let array = []
+
+        for (let i = a[0]; i <= a.slice(-1); i++) {
+            array.push(i)
+        }
+
+        return array
 
     }
 
