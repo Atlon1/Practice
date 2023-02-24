1eef2029d8ad58395f021be4fcce4b59553a0f32
Atlon1 <maciej.szajstek@gmail.com>
2023-02-24 13:53:06 +0100

    task solved

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = [[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]
+    const arr1 = "abcd"
 
 
     const result = (arr) => {
-
-        let total = 0;
-
-
-
-        for (let i = 0; i < arr.length; i++){
-            total += Math.min(...arr[i])
-        }
-
-        return total;
+        const res = arr.split('').map(c=> c+c).join('')
+        console.log(res)
 
 
     }
