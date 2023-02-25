26c265b7863bc2f7868a6b9f79c07afca6e5fdb1
Atlon1 <maciej.szajstek@gmail.com>
2023-02-25 20:41:37 +0100

    task solved

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = 'abcde'
-    const arr2 = 'cde'
+    const arr1 = 10
+    // const arr2 = "garlic naan"
 
 
-    const result = (a,b) => {
-        return a.endsWith(b)
+    const result = (a) => {
+       if (a === 1 ||a === 2 || a === 3){
+           return 1
+       } else if (a === 4 ||a=== 5 ||a===  6){
+           return 2
+       }else if (a === 7||a===  8 ||a===  9){
+           return 3
+       }else if (a === 10 ||a===  11 ||a===  12){
+           return 4
+       }
 
     }
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
     return (
         <>
