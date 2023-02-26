34a6104b0098c5dfcab827fab1a3310880a86542
Atlon1 <maciej.szajstek@gmail.com>
2023-02-26 20:42:17 +0100

    task solved

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = 10
+    const arr1 = 3
     // const arr2 = "garlic naan"
 
 
     const result = (a) => {
-       if (a === 1 ||a === 2 || a === 3){
-           return 1
-       } else if (a === 4 ||a=== 5 ||a===  6){
-           return 2
-       }else if (a === 7||a===  8 ||a===  9){
-           return 3
-       }else if (a === 10 ||a===  11 ||a===  12){
-           return 4
-       }
-
+        let array = []
+        for (let i = 0 ; i <= a; i++){
+            array[i]= []
+            for (let j = 0; j < a; j++){
+                array[i][j] = (i + 1) * (j + 1)
+            }
+        }
+        return array
     }
 
     console.log(result(arr1))
