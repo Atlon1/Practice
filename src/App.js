1c436c50684c9d7b115dbefdb0ba5936f3bbff7a
Atlon1 <maciej.szajstek@gmail.com>
2023-03-02 20:05:01 +0100

    gh rdy

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = 4
-    const arr2 = 2
+    const arr1 = true
+    const arr2 = false
     const arr3 = 6
 
 
 
 
-    const result = (a,b,c) => {
-
-        const res1 = 2*(a * b) + 2*(a * c) + 2*(b*c)
-        let res2 = (a * b)* c
-        const array = [res1, res2]
-        return array
+    const result = (a,b) => {
+        if ( a === false && b === false){
+            return false
+        } else if ( a === false && b === true) {
+            return true
+        }  else if ( a === true && b === true) {
+            return false
+        }  else if ( a === true && b === false) {
+            return true
+        }
     }
 
-   console.log(result(arr1,arr2,arr3))
+   console.log(result(arr1,arr2))
 
 
 
