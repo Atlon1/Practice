b9d60260fc28a07e9432bb71fa9504b76674e3ca
Atlon1 <maciej.szajstek@gmail.com>
2023-03-04 20:32:39 +0100

    practice rdy

--- src/App.js
+++ src/App.js
@@ -5,26 +5,39 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = true
-    const arr2 = false
-    const arr3 = 6
+    const arr1 = 12
+    const arr2 = 50
+    const arr3 = 4
+    const arr4 = 8
+    const arr5 = true
 
 
 
+    const result = (a,b,c,d,e) => {
+       if (e === true){
+           let res3 = d / 2
+           let res = b / res3
+           let res2 = a / c
+           if ( res2 < res){
+               return "Alive"
+           } else  {
+               return "Shark Bait!"
+           }
+       } else {
 
-    const result = (a,b) => {
-        if ( a === false && b === false){
-            return false
-        } else if ( a === false && b === true) {
-            return true
-        }  else if ( a === true && b === true) {
-            return false
-        }  else if ( a === true && b === false) {
-            return true
+        let res = b - d
+        let res2 = a - c
+
+        if ( res2 > res){
+            return "Alive"
+        } else  {
+            return "Shark Bait!"
         }
+       }
+
     }
 
-   console.log(result(arr1,arr2))
+   console.log(result(arr1,arr2,arr3,arr4,arr5))
 
 
 
