2fabc015e9ed5e387874533c566389efaf5ac4d0
Atlon1 <maciej.szajstek@gmail.com>
2023-03-06 21:32:02 +0100

    example rdy

--- src/App.js
+++ src/App.js
@@ -5,39 +5,24 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr1 = 12
-    const arr2 = 50
-    const arr3 = 4
-    const arr4 = 8
-    const arr5 = true
+    const arr1 = [4, 12, 3, 1, 5]
+    // const arr2 = [1,2,3,4,5,10,12]
+    // const arr3 = 4
+    // const arr4 = 8
+    // const arr5 = true
 
 
 
-    const result = (a,b,c,d,e) => {
-       if (e === true){
-           let res3 = d / 2
-           let res = b / res3
-           let res2 = a / c
-           if ( res2 < res){
-               return "Alive"
-           } else  {
-               return "Shark Bait!"
-           }
-       } else {
-
-        let res = b - d
-        let res2 = a - c
-
-        if ( res2 > res){
-            return "Alive"
-        } else  {
-            return "Shark Bait!"
+    const result = (a) => {
+        let newArr = []
+        for (let i=0; i < a.length -1; i++){
+            newArr.push(a[i]*a[i +1])
         }
-       }
-
+        return Math.max(...newArr)
     }
 
-   console.log(result(arr1,arr2,arr3,arr4,arr5))
+
+   console.log(result(arr1))
 
 
 
