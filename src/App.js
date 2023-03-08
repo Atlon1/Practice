54e316ad947064353294098a0554edba3548fe84
Atlon1 <maciej.szajstek@gmail.com>
2023-03-08 21:28:54 +0100

    example rdy

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 10
+    const arr1 = "233"
     // const arr2 = 'Phoenix'
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (nFloors) => {
-        let tower = [];
-        for (let i = 0; i < nFloors; i++) {
-            tower.push(" ".repeat(nFloors - i - 1)
-                + "*".repeat((i * 2)+ 1)
-                + " ".repeat(nFloors - i - 1));
-        }
-        return tower;
+    const result = (a) => {
+       return  a.toString()
+            .split("")
+            .reverse()
+            .map( (a, i) => a * Math.pow(10, i))
+            .filter(a => a > 0)
+            .reverse()
+            .join(" + ");
     }
 
 
