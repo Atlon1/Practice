3c45e6525f4e12ce4d3da2afdedee7a7ec667dcb
Atlon1 <maciej.szajstek@gmail.com>
2023-03-09 22:15:08 +0100

    example rdy

--- src/App.js
+++ src/App.js
@@ -5,30 +5,23 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 135
-    const arr2 = 1.92
+    const arr1 = 'riley'
+    // const arr2 = 1.92
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (a,b) => {
-       const res = (a/Math.pow(2, b)).toFixed(2)
-
-        if (res <= 18.5){
-            return  "Underweight"
-        } else if ( res <= 25){
-            return "Normal"
-        } else if ( res <= 30){
-            return "Overweight"
-        }else {
-            return  "Obese"
-        }
+    const result = (a) => {
+        const res = a.split("")
+        const res2 = res[0].toUpperCase()
+        console.log(res2)
+       return `Hello ${a}`
 
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
 
     return (
