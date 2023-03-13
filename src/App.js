17643b9a6a4e9a34dbb9a76b25694385741e53e7
Atlon1 <maciej.szajstek@gmail.com>
2023-03-13 21:03:58 +0100

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,29 +5,28 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "camelCase"
-    // const arr2 = 850
+    const arr1 = [41, 75, 72, 56, 80, 82, 81, 33]
+    const arr2 = 50
     // const arr3 = 70
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (a) => {
+    const result = (a, b) => {
+        let res = a.reduce((ele, ind) => {
+            return (ele + ind)
+        })
+        res = res / a.length
 
-    a = a.split("").map((ele)=> {
-        if (ele === ele.toUpperCase()){
-            ele = ' ' + ele
+        if (b > res) {
+            return true
+        } else {
+            return false
         }
-        return ele
-    })
-
-        return a.join('')
-
-
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1, arr2))
 
 
     return (
