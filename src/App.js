bcdfb988c0002cc5610736b0924c06930afefe8a
Atlon1 <maciej.szajstek@gmail.com>
2023-03-14 21:44:05 +0100

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,24 +5,34 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [41, 75, 72, 56, 80, 82, 81, 33]
-    const arr2 = 50
+    const arr1 = 2010
+    const arr2 = 1990
     // const arr3 = 70
     // const arr4 = 8
     // const arr5 = true
 
 
     const result = (a, b) => {
-        let res = a.reduce((ele, ind) => {
-            return (ele + ind)
-        })
-        res = res / a.length
+        if (b > a) {
+            let res = b - a
+            if (res > 2) {
+                return `You are ${res} years old.`
+            } else {
+                return `You are ${res} year old.`
+            }
 
-        if (b > res) {
-            return true
+        } else if (a > b) {
+            let res = a - b
+            if (res > 9) {
+                return `You will be born in ${res} years.`
+            } else {
+                return `You will be born in ${res} year.`
+            }
+
         } else {
-            return false
+            return "You were born this very year!"
         }
+
     }
 
 
