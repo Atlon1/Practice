89a5a21fc6c5b34dfa5bed130763c051c3ab211e
Atlon1 <maciej.szajstek@gmail.com>
2023-03-15 22:08:48 +0100

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26]
-    const arr2 = 2
+    const arr1 = [1, 3, 5, 7]
+    // const arr2 = 2
     // const arr3 = 70
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (a, b) => {
-        return a.filter((e) => e % 2 === 0).slice(-b)
+    const result = (a) => {
+        return a.reduce((a,b)=>  a + b)/a.length
     }
 
 
-    console.log(result(arr1, arr2))
+    console.log(result(arr1))
 
 
     return (
