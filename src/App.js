1b39cee49f066570c119f2e7ee39246402c02a0c
Atlon1 <maciej.szajstek@gmail.com>
2023-03-17 20:06:41 +0100

    Task rdy

--- src/App.js
+++ src/App.js
@@ -5,23 +5,16 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [ 3, 5 ]
-    // const arr2 = 2
-    // const arr3 = 70
+    const arr1 = "this is number: 7"
+    // const arr2 = 0.66
+    // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
     const result = (a) => {
-        if (a.length > 2){
-            let res = a.sort((a,b) => a - b)
-            let res2 = res.shift()
-            let res3 = res.pop()
-            return res.reduce((a,b)=> a + b)
-        } else {
-            return 0
-        }
 
+return Number(a.replace(/\D/g, ''))
 
     }
 
