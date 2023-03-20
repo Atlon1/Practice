657801dc7241bc7f190aa0b1828b68b9281ee7c3
Atlon1 <maciej.szajstek@gmail.com>
2023-03-20 20:44:32 +0100

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "this is number: 7"
-    // const arr2 = 0.66
+    const arr1 = 85
+    // const arr2 = "z"
     // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
     const result = (a) => {
-
-return Number(a.replace(/\D/g, ''))
-
+        return a === 88 ? "Leo finally won the oscar! Leo is happy" :
+            a === 86 ? "Not even for Wolf of wallstreet?!" :
+                a <= 86 ? "When will you give Leo an Oscar?" :
+                    a >= 89 ? "Leo got one already!" : "When will you give Leo an Oscar?"
     }
 
 
