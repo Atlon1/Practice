f13fc941fb031c055c243bb8a5d642a5f5242768
Atlon1 <maciej.szajstek@gmail.com>
2023-03-20 21:07:49 +0100

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 85
+    const arr1 = [9, 3, '7', '3']
     // const arr2 = "z"
     // const arr3 = 1.5
     // const arr4 = 8
@@ -13,10 +13,22 @@ function App() {
 
 
     const result = (a) => {
-        return a === 88 ? "Leo finally won the oscar! Leo is happy" :
-            a === 86 ? "Not even for Wolf of wallstreet?!" :
-                a <= 86 ? "When will you give Leo an Oscar?" :
-                    a >= 89 ? "Leo got one already!" : "When will you give Leo an Oscar?"
+        let array = []
+        const res = a.filter((n) => typeof n === "number")
+
+        if (res.length > 0) {
+           return res.reduce((e, f) => e + f)
+        }
+        const res2 = a.filter((n) => typeof n === "string").map((e) => Number(e))
+
+        if (res2.length > 0) {
+           return res2.reduce((e, f) => e + f)
+        }
+
+
+
+        console.log(array)
+
     }
 
 
