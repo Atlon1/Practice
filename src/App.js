8bde411ec1e388337060bd3f5c90177340b89823
Atlon1 <maciej.szajstek@gmail.com>
2023-04-05 23:08:48 +0200

    portfolio rdy fire base rdy

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [5,4,2,3]
-    // const arr2 = 2
+    const arr1 = 'James'
+    const arr2 = "Stevens"
     // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (a) => {
-       return  a.sort((a, b) => a - b).reduce((pre, val) => pre + val * a.pop(), 0)
+    const result = (a,b) => {
+       return  a + " " + b
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
 
     return (
