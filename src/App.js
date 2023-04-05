a8d239d34ce4af7ef4d621572f423d729380ea3b
Atlon1 <maciej.szajstek@gmail.com>
2023-04-05 23:37:40 +0200

    task redy

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'James'
-    const arr2 = "Stevens"
+    const arr1 = ["Germany", "Ukraine"]
+    const arr2 = [0,0]
     // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
     const result = (a,b) => {
-       return  a + " " + b
+       return b[0] > b[1] ? `At match ${a[0]} - ${a[1]}, ${a[0]} won!` :
+           b[0] < b[1] ? `At match ${a[0]} - ${a[1]}, ${a[1]} won!` :
+                 `At match ${a[0]} - ${a[1]}, - teams played draw.`
     }
 
 
