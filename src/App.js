5935ffad17b65123468dcba6a1aae5a67136a2d5
Atlon1 <maciej.szajstek@gmail.com>
2023-04-12 23:36:48 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ import React from "react";
 function App() {
 
 
-    const arr1 = ["Germany", "Ukraine"]
-    const arr2 = [0,0]
+    const arr1 = 'H4w 1r2 y45 t4d1y?'
+    // const arr2 = [0,0]
     // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (a,b) => {
-       return b[0] > b[1] ? `At match ${a[0]} - ${a[1]}, ${a[0]} won!` :
-           b[0] < b[1] ? `At match ${a[0]} - ${a[1]}, ${a[1]} won!` :
-                 `At match ${a[0]} - ${a[1]}, - teams played draw.`
+    const result = (a) => {
+      const res = a.split("").map((elem)=>{
+          if (elem === '1') {
+              return elem = 'a'
+          } else if (elem === '2'){
+              return elem = 'e'
+          } else if (elem === '3'){
+              return elem = 'i'
+          } else if (elem === '4'){
+              return elem = 'o'
+          }else if (elem === '5'){
+              return elem = 'u'
+          } else {
+              return elem
+          }
+      }).join('')
+
+        return res
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
 
     return (
