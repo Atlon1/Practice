d70d936e3c042a28f097a9f9354dc3a652859d59
Atlon1 <maciej.szajstek@gmail.com>
2023-04-13 21:25:09 +0200

    task ready

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import React from "react";
 function App() {
 
 
-    const arr1 = ['what', 'a', 'great', 'kata']
-    const arr2 = 'a'
+    const arr1 = [-5,-1,-6,-18]
+    const arr2 = 3
     // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
     const result = (a,b) => {
-        return a.includes(b)
+        const res = a.sort((a,b)=>a - b)
+        return res[b-1]
     }
 
 
