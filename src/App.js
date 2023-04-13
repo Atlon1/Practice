6df46c2624454ffaf95b3e0cdb7599b1016aa652
Atlon1 <maciej.szajstek@gmail.com>
2023-04-13 20:59:39 +0200

    task ready

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'H4w 1r2 y45 t4d1y?'
+    const arr1 = [1,1,2]
     // const arr2 = [0,0]
     // const arr3 = 1.5
     // const arr4 = 8
@@ -13,23 +13,7 @@ function App() {
 
 
     const result = (a) => {
-      const res = a.split("").map((elem)=>{
-          if (elem === '1') {
-              return elem = 'a'
-          } else if (elem === '2'){
-              return elem = 'e'
-          } else if (elem === '3'){
-              return elem = 'i'
-          } else if (elem === '4'){
-              return elem = 'o'
-          }else if (elem === '5'){
-              return elem = 'u'
-          } else {
-              return elem
-          }
-      }).join('')
-
-        return res
+      return a.filter((item, index) => a.indexOf(item) === index)
     }
 
 
