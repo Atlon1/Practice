faec55eb54b6d1cd124859844462261a7e431e9f
Atlon1 <maciej.szajstek@gmail.com>
2023-04-13 21:19:32 +0200

    task ready

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [1,1,2]
-    // const arr2 = [0,0]
+    const arr1 = ['what', 'a', 'great', 'kata']
+    const arr2 = 'a'
     // const arr3 = 1.5
     // const arr4 = 8
     // const arr5 = true
 
 
-    const result = (a) => {
-      return a.filter((item, index) => a.indexOf(item) === index)
+    const result = (a,b) => {
+        return a.includes(b)
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
 
     return (
