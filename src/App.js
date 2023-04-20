14b8a87a9d14d5e88ad81c570456bf114676ae27
Atlon1 <maciej.szajstek@gmail.com>
2023-04-20 20:33:14 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,25 +5,32 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 1000
-     const arr2 = 0.05
-     const arr3 = 0.18
-     const arr4 = 1200
+    const arr1 = "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"
+     // const arr2 = 0.05
+     // const arr3 = 0.18
+     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b,c,d) => {
-        let year = 0
-        while (a < d){
-            a += a * b * (1 -c)
-            year += 1
-        }
+    const result = (a) => {
+        return a.split('\n')
+            .map(line => line.split('').reverse().join(''))
+            .join('\n')
+    }
 
-        return year
+    const result2 = (a) => {
+        return a.split('\n')
+            .reverse()
+            .join('\n')
+    }
+    const result3 = (fct, s) => {
+        return fct(s)
     }
 
 
-    console.log(result(arr1,arr2,arr3,arr4))
+    console.log(result(arr1))
+    console.log(result2(arr1))
+
 
 
     return (
