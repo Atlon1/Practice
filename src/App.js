f6a9a43b1da5407b50864fe49f7e2745156e08ca
Atlon1 <maciej.szajstek@gmail.com>
2023-04-20 20:43:00 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,32 +5,26 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"
-     // const arr2 = 0.05
-     // const arr3 = 0.18
-     // const arr4 = 1200
+    const arr1 = ""
+    // const arr2 = 0.05
+    // const arr3 = 0.18
+    // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a) => {
-        return a.split('\n')
-            .map(line => line.split('').reverse().join(''))
-            .join('\n')
-    }
 
-    const result2 = (a) => {
-        return a.split('\n')
-            .reverse()
-            .join('\n')
-    }
-    const result3 = (fct, s) => {
-        return fct(s)
+        const res = a.length
+
+        if (0 < res) {
+            return a.split(' ').map(line => line = "sex").join(' ')
+        } else return ""
+
+
     }
 
 
     console.log(result(arr1))
-    console.log(result2(arr1))
-
 
 
     return (
