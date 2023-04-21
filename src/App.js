0b183c48995bd0c6e72c807a03080cc64eef66f4
Atlon1 <maciej.szajstek@gmail.com>
2023-04-21 21:18:54 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import React from "react";
 function App() {
 
 
-    const arr1 = ""
-    // const arr2 = 0.05
+    const arr1 = -456
+    const arr2 = 3
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
+    const result = (a,b) => {
+        const res = a.toString().split('').reverse()
 
-        const res = a.length
-
-        if (0 < res) {
-            return a.split(' ').map(line => line = "sex").join(' ')
-        } else return ""
-
-
+        if (b <= 0){
+            return -1
+        } else if (res.length < b ){
+            return 0
+        } else if (res.length >= b){
+            let res3 = parseInt(Number(res[b - 1]))
+            if (isNaN(res3)){
+                return 0
+            }
+            return res3
+        }
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
 
     return (
