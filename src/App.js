69bb193f57780add546bdd4c90763c85c5ab8e62
Atlon1 <maciej.szajstek@gmail.com>
2023-04-21 21:39:39 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "CodeWars"
-    // const arr2 = 3
+    const arr1 = [1, 2, 3]
+    const arr2 = 2
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-        const even = a.split('').filter((v,i) => i % 2 === 0).join('')
-        const odd = a.split('').filter((v,i) => i % 2 !== 0).join('')
-
-
-        return `${even} ${odd}`
+    const result = (a,b) => {
+        return a.map((elem) => elem * b)
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
 
     return (
