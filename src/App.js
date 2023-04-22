f7995d4eaf159c7f4d03de3e3faacbcd64723cae
Atlon1 <maciej.szajstek@gmail.com>
2023-04-22 20:52:23 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [1, 2, 3]
-    const arr2 = 2
+    const arr1 = [3, 4, 4, 3, 6, 3]
+    // const arr2 = 2
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-        return a.map((elem) => elem * b)
+    const result = (a) => {
+        return a.filter((val,i) => a.lastIndexOf(val) == i)
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
 
     return (
