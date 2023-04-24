a96408bb0785f7bd49efbe90adf61774148335c8
Atlon1 <maciej.szajstek@gmail.com>
2023-04-24 20:05:01 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'string1'
-    const arr2 = 'string2'
+    const arr1 = 1000000
+    // const arr2 = 'string2'
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-       return `${a} ${b}`
+    const result = (a) => {
+        let paper = 0.0001;
+        let folds = 0;
+        if (a <= 0) {
+            return null;
+        } else {
+            while ( a > paper ) {
+                paper *= 2;
+                folds += 1;
+            }
+        }
+        return folds;
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
 
     return (
