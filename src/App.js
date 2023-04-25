11ff6c6ef48737192540f829deeb40e6af16fa9d
Atlon1 <maciej.szajstek@gmail.com>
2023-04-25 21:34:32 +0200

    task seaarch value in loop for to 3 max value

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'pqksuvy'
+    const arr1 = ["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]
     // const arr2 = '222'
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,7 +13,17 @@ function App() {
 
 
     const result = (a) => {
-       return a.split('').sort().join('')
+       let bag = []
+
+        for (let i = 0;i < a.length;i++){
+            if (a[i] ===  "Barbie doll" || a[i] === "Hello Kitty" ){
+                bag.push(a[i])
+            } else continue
+            if (bag.length ===3){
+                break
+            }
+        }
+        return bag
     }
 
 
