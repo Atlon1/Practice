ec2b78ab28b88dac9f1ec6723c9cf6ed1c237580
Atlon1 <maciej.szajstek@gmail.com>
2023-04-25 21:27:02 +0200

    task alfabethic sort string

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"
+    const arr1 = 'pqksuvy'
     // const arr2 = '222'
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,12 +13,7 @@ function App() {
 
 
     const result = (a) => {
-        const string = a.match(/[a-z]/g).length
-        const stringUp = a.match(/[A-Z]/g).length
-        const numeber = a.match(/[0-9]/g).length
-        const special = a.match(/[^A-Z0-9]/gi).length
-
-        return [stringUp,string,numeber,special]
+       return a.split('').sort().join('')
     }
 
 
