ec6740354dcd44e5e83f34c1aa276bfd069a8c1a
Atlon1 <maciej.szajstek@gmail.com>
2023-04-25 21:23:31 +0200

    task rdy mach string number special

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 222
-    const arr2 = '222'
+    const arr1 = "RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"
+    // const arr2 = '222'
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-        return a === parseInt(b)
+    const result = (a) => {
+        const string = a.match(/[a-z]/g).length
+        const stringUp = a.match(/[A-Z]/g).length
+        const numeber = a.match(/[0-9]/g).length
+        const special = a.match(/[^A-Z0-9]/gi).length
+
+        return [stringUp,string,numeber,special]
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
 
     return (
