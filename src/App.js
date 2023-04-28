8cc5b63b57d921c8496a62879a6cc1d767058c1c
Atlon1 <maciej.szajstek@gmail.com>
2023-04-28 20:27:47 +0200

    zadanie oddzielajace zdanie po przecinku

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import React from "react";
 function App() {
 
 
-    const arr1 = ["apple","banana","rottenKiwi","melone","orange"]
-    // const arr2 = '\C'
+    const arr1 ="Tuesday January 29, 10pm"
+    // const arr2 = 1.23
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a) => {
-       const res = a.map((elem) => elem.replace('rotten', '').toLowerCase())
-
+       const res = a.split(',')[0]
         return res
     }
-
 
     console.log(result(arr1))
 
