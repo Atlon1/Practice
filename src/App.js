ce0ffacf94ad942ebffe0fd081a16e596697f4cc
Atlon1 <maciej.szajstek@gmail.com>
2023-04-28 21:25:13 +0200

    zadanie wymiana 2 znakow na inne

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 ="Tuesday January 29, 10pm"
+    const arr1 ='aaabcccbaaa'
     // const arr2 = 1.23
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,8 +13,18 @@ function App() {
 
 
     const result = (a) => {
-       const res = a.split(',')[0]
-        return res
+       const res = a.split('')
+        const arr = []
+
+        for (let i = 0; i < res.length; i++){
+            if (res[i] === 'a'){
+                arr.push('b')
+            } else if (res[i]=== "b"){
+                arr.push('a')
+            } else arr.push(res[i])
+        }
+
+        return arr.join('')
     }
 
     console.log(result(arr1))
