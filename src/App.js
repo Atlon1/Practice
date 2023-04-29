e4b3640ce406ae84b38c8648f16a391c92bacacc
Atlon1 <maciej.szajstek@gmail.com>
2023-04-29 21:44:13 +0200

    task if rdy

--- src/App.js
+++ src/App.js
@@ -5,25 +5,18 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "abcdef"
-    const arr2 = [1, 2, 5]
+    const arr1 = 1.01
+    // const arr2 = [1, 2, 5]
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a, b) => {
-        const res = a.split('')
-
-        for (let i = 0; i < b.length; i++) {
-            if(res[b[i]]){
-                res[b[i]] = res[b[i]].toUpperCase()
-            }
-        }
-        return res.join('')
+    const result = (a) => {
+        return a >= 1.01 ? "F" : a >= 0.9 ? "A" : a >= 0.8 ? "B" : a >= 0.7 ? "C" : a >= 0.6 ? "D" :  "F"
     }
 
-    console.log(result(arr1, arr2))
+    console.log(result(arr1))
 
 
     return (
