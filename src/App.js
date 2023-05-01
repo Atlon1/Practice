6d13da4f95011bb0d4ec1cb0bb42813a0d3ee1db
Atlon1 <maciej.szajstek@gmail.com>
2023-05-01 08:48:36 +0200

    zadanie obliczajace sume punktow, wg poycji w alfabecie

--- src/App.js
+++ src/App.js
@@ -5,21 +5,17 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [-0, 1, 2, -3, 4, 5, -6]
+    const arr1 = "attitude"
     // const arr2 = [1, 2, 5]
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-      const res = a.sort((a,b) => a-b)
-        if (a.length <= 1){
-            return 0
-        } else {
-            const res2 = res[res.length - 1] -res[0]
-            return res2
-        }
+    const result = (string) => {
+        return string.split('').reduce((acc, char) => {
+            return acc + char.charCodeAt() - 96;
+        }, 0);
 
     }
 
