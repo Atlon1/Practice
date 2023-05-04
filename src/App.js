f7e170d42305d0a2b90664f4c47d8039eb50cc56
Atlon1 <maciej.szajstek@gmail.com>
2023-05-04 20:33:10 +0200

    dodanie separatora

--- src/App.js
+++ src/App.js
@@ -6,23 +6,21 @@ function App() {
 
 
 
-    const arr1 = 4
-    const arr2 = 2
+    const arr1 = "My name is John"
+    const arr2 = "-"
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
 
-    const result = (a, b) => {
-       let res = 1
-        for (let i = 1; i <= b; i++){
-            res = res * a
-        }
-            return res
+    const result = (a,b) => {
+        const res = a.split(' ').map((elem) => {
+            return elem.split('').join(b)}).join(' ')
+        return res
     }
 
-    console.log(result(arr1, arr2))
+    console.log(result(arr1,arr2))
 
 
     return (
