ba41dc71df15aff58e831185eceaf3e5dd701eaa
Atlon1 <maciej.szajstek@gmail.com>
2023-05-07 22:09:07 +0200

    task redy

--- src/App.js
+++ src/App.js
@@ -5,33 +5,25 @@ import React from "react";
 function App() {
 
 
-
-    const arr1 = 0
-    const arr2 = 3
+    const arr1 = 6
+    // const arr2 = 3
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-
-    const result = (a,b) => {
-        if (a <= 0 || b <= 0){
-            return []
-        } else {
-
-            const arr =[a]
-
-            for (let i = 0; b-1 > i; i++){
-                let res = arr[arr.length - 1]*arr[arr.length - 1]
-                arr.push(res)
-            }
-            return arr
+    const result = (a) => {
+        let sum = 0;
+        for(let i = 1; i <= a; i++)
+        {
+            sum += (i*(i+1))/2;
         }
+        return sum;
 
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
     return (
         <>
