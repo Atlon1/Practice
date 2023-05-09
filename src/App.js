bfb15640494704e658e5ddc86707e205001239ac
Atlon1 <maciej.szajstek@gmail.com>
2023-05-09 21:52:56 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "a"
-    const arr2 = 2
+    const arr1 = [10,2,3,8,1,10,4]
+    const arr2 = 5
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a,b) => {
-        return typeof(a + b)
+        return  a.sort((a,b)=> a - b).reverse().slice(0, b).reduce((a,b) => a *b)
     }
 
 
