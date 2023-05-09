176d6f69621b303e5fb9a43f4e095741e7c44045
Atlon1 <maciej.szajstek@gmail.com>
2023-05-09 21:41:21 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'C.....m'
-    // const arr2 = 3
+    const arr1 = "a"
+    const arr2 = 2
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-        const res = a.split('')
-        if (res.filter((elem)=> elem === ".").length > 3){
-            return "Escaped!"
-        } else
-
-        return "Caught!"
+    const result = (a,b) => {
+        return typeof(a + b)
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
     return (
         <>
