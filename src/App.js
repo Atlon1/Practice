6c43547ac396d780308cdb7513c3ae9611276846
Atlon1 <maciej.szajstek@gmail.com>
2023-05-11 22:02:48 +0200

    zadnie okreslajace polozenie w tabeli(includes), oraz index of

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = ["O", "O", "X", "X", "X", "X"]
+    const arr1 = ["X", "X", "X", "O", "X", "X"]
     // const arr2 = 5
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,19 +13,7 @@ function App() {
 
 
     const result = (a) => {
-        let res = 0
-        let count = -1
-
-        for (let i = 0; i <= a.length; i++) {
-            if (a[i] === "O") {
-                res += i
-                count += 1
-            } else if (res >= 0) {
-                break
-            }
-        }
-
-        return !count > 0 ? "None available!" : res
+        return !a.includes('O') ? "None available!" : a.indexOf('O')
 
     }
 
