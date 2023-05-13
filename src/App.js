fcf2ca7da206a323770d9b029c3dd801204eea61
Atlon1 <maciej.szajstek@gmail.com>
2023-05-13 23:12:31 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'abcdef'
+    const arr1 = 'Ryan'
     // const arr2 = 5
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,14 +13,13 @@ function App() {
 
 
     const result = (a) => {
-        let total1 = ''
-        for (let i = 0; i < a.length; i++) {
-            let res = a.charCodeAt(i)
-            total1 += `${res}`
-        }
-
-        let total2 = total1.split('').map((elem) => elem === "7" ? "1" : elem).join('')
-        return parseInt(total1 - total2)
+      if (a === String){
+          return "Who ate the last cookie? It was Zach!"
+      } else if (a === Number){
+          return "Who ate the last cookie? It was Monica!"
+      } else if (a === Boolean){
+          return  "Who ate the last cookie? It was the dog!"
+      }
     }
 
 
