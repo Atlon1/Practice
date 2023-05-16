c29cb3c9cd720aad796d5a23bde8040df8484918
Atlon1 <maciej.szajstek@gmail.com>
2023-05-16 20:48:29 +0200

    Przywitania

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "Example Input"
+    const arr1 = "lA ArE? wiE! are bien: hombRe vISta DOIng arE bIen hallo, wIE hoW; TsChUSS? tRes YOU"
     // const arr2 = 5
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-        if (a >= b) return "INVALID";
-
-        var sum = 0;
-        for (var i = a; i < b; i+=a) {
-            sum += i;
+    const result = (a) => {
+        const res = a.split(' ').map(elem => elem.toLowerCase()).join(' ')
+        if (res.includes('salut')) {
+            return true
+        } else if (res.includes('ciao')) {
+            return true
+        } else if (res.includes('hello')) {
+            return true
+        } else if (res.includes('hallo')) {
+            return true
+        } else if (res.includes('hola')) {
+            return true
+        } else if (res.includes('ahoj')) {
+            return true
+        }else if (res.includes('czesc')) {
+            return true
         }
-        return sum;
+
+        else return false
+
     }
 
 
