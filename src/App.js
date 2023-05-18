e2d32eae9c180c14e0c6b1024daccceb47f1a6cb
Atlon1 <maciej.szajstek@gmail.com>
2023-05-18 21:20:58 +0200

    Zadanie ktore usowa z 1 tablicy wszystkie wartosci z 2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "hello world"
-    // const arr2 = 5
+    const arr1 = [1, 1, 2, 3, 1, 2, 3, 4]
+    const arr2 = [1, 3]
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-        return a.split('').map(elem => elem +" ").join('').split('').slice(0,-1).join('')
+    const result = (a,b) => {
+       return a.filter(elem => b.lastIndexOf(elem) === -1)
 
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
     return (
         <>
