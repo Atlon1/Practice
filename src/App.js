b5fc8af54c4bf22ba6cb9863c6030a371c0b2e20
Atlon1 <maciej.szajstek@gmail.com>
2023-05-19 23:59:57 +0200

    zadnie kotre zwraca wartosc najmniejsza lub indeks w zaleznosci od 2 parametru

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "How can mirrors be real if our eyes aren't real"
-    // const arr2 = [1, 3]
+    const arr1 = [-40, -20, 320, 350, 500]
+    const arr2 = 'value'
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-        return a.split(' ').map((elem) => {
-           elem.split('')
-           return  elem[0].toUpperCase() + elem.slice(1)
-        }).join(' ')
+    const result = (a, b) => {
+        if (b === "value") {
+            a.sort((a, b) => a - b)
+            return a[0]
+        } else if (b === "index") {
+            a.sort((a, b) => a - b)
+            return a.indexOf(1)
+        }
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1, arr2))
 
     return (
         <>
