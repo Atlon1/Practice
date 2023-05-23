6937f1d3ef2a56c7690c3a39b8e4f77b3663ceef
Atlon1 <maciej.szajstek@gmail.com>
2023-05-23 22:13:12 +0200

    zadanie z wstawieniem spacji wg 2 param w funkcji

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'this is my string'
-    const arr2 = 'value'
+    const arr1 = "supercalifragilisticexpialidocious"
+    const arr2 = 3
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-     return a.replace(/[aeiou]/ig, (m,i) => i + 1)
+    const result = (a,b) => {
+        let result = [];
+
+        for (let i = 0; i < a.length; i += b) {
+            result.push(a.slice(i, b + i));
+        }
+        return result.join(' ')
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
     return (
         <>
