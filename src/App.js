d3aad3140b06d38e8d7bb091f792f2d1cc8ca7e2
Atlon1 <maciej.szajstek@gmail.com>
2023-05-23 22:36:46 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "supercalifragilisticexpialidocious"
-    const arr2 = 3
+    const arr1 = [0, 1, 2, 2, 3]
+    const arr2 = 2
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a,b) => {
-        let result = [];
-
-        for (let i = 0; i < a.length; i += b) {
-            result.push(a.slice(i, b + i));
-        }
-        return result.join(' ')
+       return a.filter((elem) => elem === b).length
     }
 
 
