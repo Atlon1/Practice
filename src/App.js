b98241d8acbe4bb2c6510f44850acbfa3acbb91e
Atlon1 <maciej.szajstek@gmail.com>
2023-05-25 21:30:44 +0200

    zadanie sprawdzajace ile w danym roku jest piatkow 13

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [0, 1, 2, 2, 3]
-    const arr2 = 2
+    const arr1 = 2792
+    // const arr2 = 2
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-       return a.filter((elem) => elem === b).length
+    const result = (a) => {
+      let unlocky = 0
+
+        for(let i = 0; i <12;i++){
+            if(new Date(a,i,13).getDay() === 5){
+                unlocky ++
+            }
+        }
+
+        return unlocky
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
     return (
         <>
