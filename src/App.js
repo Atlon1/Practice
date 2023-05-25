6c0b6365e86e68f12069c9418ebcb3d6b835dc12
Atlon1 <maciej.szajstek@gmail.com>
2023-05-25 21:36:40 +0200

    zadanie polegajace na liczeniu przerw pomiedzy liczbami z wybranego stringu

--- src/App.js
+++ src/App.js
@@ -5,27 +5,20 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 2792
-    // const arr2 = 2
+    const arr1 = "ababc"
+    const arr2 = 'c'
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a) => {
-      let unlocky = 0
-
-        for(let i = 0; i <12;i++){
-            if(new Date(a,i,13).getDay() === 5){
-                unlocky ++
-            }
-        }
-
-        return unlocky
+    const result = (a,b) => {
+        a = a.toLowerCase()
+        return a.includes(b) ? a.lastIndexOf(b) - a.indexOf(b) : -1
     }
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
     return (
         <>
