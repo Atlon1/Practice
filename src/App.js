f2e95c8b77abc429f6c0a52895289d1cf8afad0b
Atlon1 <maciej.szajstek@gmail.com>
2023-05-26 21:31:18 +0200

    zadanie z policzeniem wszyskich liczb w stringu

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "AD"
-    const arr2 = 'BC'
+    const arr1 = "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"
+    // const arr2 = 'BC'
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-        if (/^\D+$/gi.test(a) && /^\D+$/gi.test(b)){
-            return a.split('').map(n => n.toUpperCase().charCodeAt()).reduce((a,b)=> a+b) ===
-                b.split('').map(n=>n.toUpperCase().charCodeAt()).reduce((a,b) => a+b)
-        }
+    const result = (a) => {
+        let res = a.replace(/\D/gi,' ').split(' ').map(val => Number(val)).reduce((a,b) => a + b)
+
+        return res
     }
 
 
-    console.log(result(arr1,arr2))
+    console.log(result(arr1))
 
     return (
         <>
