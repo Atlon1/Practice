2a738234cea4f18c6b7276c678488906039ef80b
Atlon1 <maciej.szajstek@gmail.com>
2023-05-30 22:43:25 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"
+    const arr1 = "Lets go to the movies"
     // const arr2 = 25
     // const arr3 = 0
     // const arr4 = 1200
@@ -13,7 +13,7 @@ function App() {
 
 
     const result = (a) => {
-        return a.replace(/Fire/gi, "~~")
+        return a.replaceAll(' ', "").toUpperCase().split('').join('  ')
     }
 
 
