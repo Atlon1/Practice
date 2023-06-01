0bc97a9dde4ef1674616e2839780c8abda339d2f
Atlon1 <maciej.szajstek@gmail.com>
2023-06-01 22:27:30 +0200

    komponent który tworzy tabeli usuwa poszczegolne wartosi oraz dodaje nowe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 
 
 function App() {
 
 
-    const arr1 = " glasses of wine and 2 shot"
+    const [arr1, setArr1] = useState([1, 2, 3, 4, 5, 6])
     // const arr2 = 25
     // const arr3 = 0
     // const arr4 = 1200
@@ -13,17 +13,36 @@ function App() {
 
 
     const result = (a) => {
-        let res = a.replace(/\D/g, ' ').split(' ').map(val => Number(val)).reduce((a,b) => a+b)
-        return res <= 1 ? `${res} glass of water` : `${res} glasses of water`
-
     }
 
 
-    console.log(result(arr1))
+    const [tableData, setTableData] = useState([1,2,3,4,5]);
+    const [date, setDate] = useState("")
+
+    const handleDelete = (index) => {
+        const update = [...tableData]
+        update.splice(index, 1)
+        setTableData(update)
+    };
+
+    const refreshDate = () => {
+       setTableData([...tableData, date])
+    }
+
 
     return (
-        <>
-        </>
+        <div>
+            <input onChange={(e) => setDate(e.target.value)} type='text'/>
+            <button onClick={() => refreshDate()}>Dodaj treść</button>
+            <ul>
+                {tableData.map((value, index) => (
+                    <li key={index}>
+                        {value}
+                        <button onClick={() => handleDelete(index)}>Usuń</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
