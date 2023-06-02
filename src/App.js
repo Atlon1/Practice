bf2700e652878055a4ce446886f96cfa197ce260
Atlon1 <maciej.szajstek@gmail.com>
2023-06-02 23:01:46 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = 1
+    const arr2 = 122
     // const arr3 = 0
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a) => {
-        const res = a.toString().split('')
-
-        return res.length <= 1 ? `Value is 0000${a}` :
-            res.length <= 2 ? `Value is 000${a}` :
-                res.length <= 3 ? `Value is 00${a}` :
-                res.length <= 4 ? `Value is 0${a}` : `Value is ${a}`
+        return `Value is ` + ("00000" + a).slice(-5)
 
     }
 
