c8b19431fe0d66e22517d5a0f19d0d5057efc923
Atlon1 <maciej.szajstek@gmail.com>
2023-06-02 23:11:56 +0200

    zadnie z wspolgloskiami ktore wyszukuje ile ich jest w zdaniu

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = 122
+    const arr2 = "codewarriors"
     // const arr3 = 0
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a) => {
-        return `Value is ` + ("00000" + a).slice(-5)
+            return a.replace(/[^aeiou]/g, " ").split(' ').sort((a, b) => b.length - a.length)[0].length
 
     }
 
