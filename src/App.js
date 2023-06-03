e7e8284e8f8f3c146a95c9f497ee18b175bab7dc
Atlon1 <maciej.szajstek@gmail.com>
2023-06-03 21:40:40 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = ["foo", "bar"]
+    const arr2 = 10
     // const arr3 = 0
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a) => {
-            return arguments.length
+            let res = 0
+            let res2 = 0
+        for (let i = 0; i<= a; i++){
+            res += i
+        }
+        res =  res*res
 
+        for (let i = 0; i<= a; i++){
+            res2 += Math.pow(i, 2)
+        }
+
+
+        return res - res2
     }
 
     console.log(result(arr2))
