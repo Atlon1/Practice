905b0f8da12707dc08b5d3242685b73649bb14e2
Atlon1 <maciej.szajstek@gmail.com>
2023-06-03 21:21:40 +0200

    liczy ile argumentow znajduje sie w zmiennej

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = "codewarriors"
+    const arr2 = ["foo", "bar"]
     // const arr3 = 0
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a) => {
-            return a.replace(/[^aeiou]/g, " ").split(' ').sort((a, b) => b.length - a.length)[0].length
+            return arguments.length
 
     }
 
