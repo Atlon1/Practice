d1f50152e3354f4b68b5143041bbb507dea80c93
Atlon1 <maciej.szajstek@gmail.com>
2023-06-04 23:09:21 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = 10
-    // const arr3 = 0
-    // const arr4 = 1200
+    const arr2 = -6
+    const arr3 = -5
+    const arr4 = 0
     // const arr5 = true
 
 
-    const result = (a) => {
-
+    const result = (a,b,c) => {
+        if ((a + c) > (b+c)){
+            return 1
+        } else if ((a + c) > (b+c)){
+            return -1
+        } else
+            return 0
     }
 
-    console.log(result(arr2))
+    console.log(result(arr2,arr3,arr4))
 
     return (
         <div>
