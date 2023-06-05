4853eaad4768e54f02c0533397b7260be4b1a73d
Atlon1 <maciej.szajstek@gmail.com>
2023-06-05 20:36:45 +0200

    zadanie budujace ciag znakow i wyznacza najdluzszy mozliwy ciag

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ function App() {
 
     const arr2 = -6
     const arr3 = -5
-    const arr4 = 0
+    // const arr4 = 0
     // const arr5 = true
 
 
-    const result = (a,b,c) => {
-        if ((a + c) > (b+c)){
-            return 1
-        } else if ((a + c) > (b+c)){
-            return -1
-        } else
-            return 0
+    const result = (strarr, k) => {
+        if (k <= 0 || k > strarr.length) {
+            return "";
+        }
+
+        return strarr
+            .map((value, index) => (
+                strarr.slice(index, index+k).join('')
+            ))
+            .reduce((longest, current) => current.length > longest.length ? current : longest)
     }
 
-    console.log(result(arr2,arr3,arr4))
+    console.log(result(arr2,arr3))
 
     return (
         <div>
