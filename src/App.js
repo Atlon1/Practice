e9790a6bac71ded3e88e04d9c58415f6c626d323
Atlon1 <maciej.szajstek@gmail.com>
2023-06-08 21:51:46 +0200

    zadanie wyszukujace duplikaty parzyscie i nie parzyscie

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = NaN
+    const arr2 = [82,91,72,76,76,100,85]
     // const arr3 = 2
     // const arr4 = 0
     // const arr5 = true
 
 
     const result = (a) => {
-      return Number.isNaN()
+      const odd = a.filter((elem, index, array) => {
+          return  (array.indexOf(elem) === index) % 2 === 0
+      }).sort((a,b) => a - b)
+
+        const even = a.filter((elem, index, array) => {
+          return  (array.indexOf(elem) === index) % 2 !== 0
+      }).sort((a,b) => a - b).reverse()
+
+
+        return [...odd, ...even]
     }
 
     console.log(result(arr2))
