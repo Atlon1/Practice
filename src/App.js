94b72aa8186b3ed8b6c4473b688ee94d42ec4043
Atlon1 <maciej.szajstek@gmail.com>
2023-06-08 21:33:49 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = ["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"]
-    const arr3 = 2
+    const arr2 = NaN
+    // const arr3 = 2
     // const arr4 = 0
     // const arr5 = true
 
 
-    const result = (a,b) => {
-      return `name: ${a[b-1]}`
+    const result = (a) => {
+      return Number.isNaN()
     }
 
-    console.log(result(arr2,arr3))
+    console.log(result(arr2))
 
     return (
         <div>
