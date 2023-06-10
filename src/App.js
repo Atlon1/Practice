495189f87ed2007486e35dbafdc9198307c57309
Atlon1 <maciej.szajstek@gmail.com>
2023-06-10 23:23:42 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = ['a', 'b', 'c']
-    const arr3 = [1, 2, 3,4]
+    const arr2 = "This Is A Test"
+    // const arr3 = [1, 2, 3,4]
     // const arr4 = 0
     // const arr5 = true
 
 
-    const result = (a, b) => {
-
-        const obj = {}
-        for (let i = 0; i < a.length; i++) {
-            obj[a[i]] = i < b.length ? b[i] : null
-        }
-
-        return obj
+    const result = (a) => {
+        return a.split(' ').map(elem => elem[0]).join('')
     }
 
-    console.log(result(arr2, arr3))
+    console.log(result(arr2))
 
     return (
         <div>
