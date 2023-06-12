9e8acc5f5fbbe0a60b0fbc998bb4ce1a4b407f7c
Atlon1 <maciej.szajstek@gmail.com>
2023-06-12 21:24:53 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = ["a", "a", "a", "b", "b"]
+    const arr2 = ['one', 'two', 'three' ]
     // const arr3 = 132
     // const arr4 = 0
     // const arr5 = true
 
 
     const result = (a) => {
-        let as = a.filter(x =>x === 'a').length;
-        let bs = a.filter(x =>x === 'b').length;
-        let cs = a.filter(x =>x === 'c').length;
-        return (as === 3 || bs === 3 || cs=== 3)
-            && (as === 2 || bs === 2 || cs === 2);
+        return a.sort()
     }
 
     console.log(result(arr2))
