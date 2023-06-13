1d935f8c27b7007007aa834245ece5234c13d656
Atlon1 <maciej.szajstek@gmail.com>
2023-06-13 21:18:45 +0200

    tasl rdy

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = [45880,27720,61759,61593,77335,44707,85100,80559,67468,61678,87143,93988,33480,23217,88804,27430,598,33815,9406,70858,26019,41035,22382,59102,38986,25309,86254,25200,8563,13311,48499,52337,39791,37994,23486,26400,1700]
-    // const arr3 = 132
-    // const arr4 = 0
+    const arr2 = [["_","_","_","_","@","_","_","_","@","_","_","_","_","@","@"],["_","_","_","_","_","_","_","_","_","_","@","_","@","@","_"],["@","_","_","@","@","_","_","_","_","_","_","_","_","_","_"]]
+    const arr3 = 6
+    const arr4 = 3
     // const arr5 = true
 
 
-    const result = (a) => {
-        let max = a.sort((a,b) => a - b).reverse().filter((elem,ind,array) => array.indexOf(elem) === ind)
-
-        return a.length >= 2 ? [max[0], max[1]] :
-                a.length === 1 ? [max[0]] : []
+    const result = (a,b,c) => {
+       let res = 0
+        let dog = 0
+        for ( let i = 0; i < a.length; i++){
+            res += a[i].filter((elem) => elem === "@").length
+            dog += a[i].filter((elem) => elem === "D").length
+        }
+        console.log(res)
+        if ( dog > 0) {
+            return"Dog!!"
+        } else  if(res <= (b * c)){
+            return "Clean"
+        } else {
+            return "Cr@p"
+        }
     }
 
-    console.log(result(arr2))
+    console.log(result(arr2,arr3,arr4))
 
     return (
         <div>
