c86854992c48f699594043b0ab6a92990d6abba0
Atlon1 <maciej.szajstek@gmail.com>
2023-06-13 21:26:22 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,30 +5,17 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = [["_","_","_","_","@","_","_","_","@","_","_","_","_","@","@"],["_","_","_","_","_","_","_","_","_","_","@","_","@","@","_"],["@","_","_","@","@","_","_","_","_","_","_","_","_","_","_"]]
-    const arr3 = 6
-    const arr4 = 3
+    const arr2 = "Nike"
+    // const arr3 = 6
+    // const arr4 = 3
     // const arr5 = true
 
 
-    const result = (a,b,c) => {
-       let res = 0
-        let dog = 0
-        for ( let i = 0; i < a.length; i++){
-            res += a[i].filter((elem) => elem === "@").length
-            dog += a[i].filter((elem) => elem === "D").length
-        }
-        console.log(res)
-        if ( dog > 0) {
-            return"Dog!!"
-        } else  if(res <= (b * c)){
-            return "Clean"
-        } else {
-            return "Cr@p"
-        }
+    const result = (a) => {
+        return   a === null ? null :  a.length >= 1 ? `hello ${a}!` : null
     }
 
-    console.log(result(arr2,arr3,arr4))
+    console.log(result(arr2))
 
     return (
         <div>
