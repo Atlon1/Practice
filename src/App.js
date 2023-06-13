395e1963ebd4251b663a5f514bdd5bdc7e390eec
Atlon1 <maciej.szajstek@gmail.com>
2023-06-13 21:51:16 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = "Nike"
+    const arr2 = 13
     // const arr3 = 6
     // const arr4 = 3
     // const arr5 = true
 
 
     const result = (a) => {
-        return   a === null ? null :  a.length >= 1 ? `hello ${a}!` : null
+        let res = 1
+        for (let i = 1; i <= a ; i++){
+            res *= i
+        }
+
+        return Math.ceil(((res - 1) + 1)/(a*a)) === a ? true : false
     }
 
     console.log(result(arr2))
