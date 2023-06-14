0b027be8e64ac499bdcec14b4bf41dac81805800
Atlon1 <maciej.szajstek@gmail.com>
2023-06-14 21:48:13 +0200

    zadanie sprawdzajce czy sasiadujace cyfry sa wiesze od 1

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = 13
+    const arr2 = 32
     // const arr3 = 6
     // const arr4 = 3
     // const arr5 = true
 
 
     const result = (a) => {
-        let res = 1
-        for (let i = 1; i <= a ; i++){
-            res *= i
+       const res = a.toString().split('')
+        for(let i = 0; i < res.length-1;i++){
+            if (Math.abs(res[i] - res[i + 1] !== 1)){
+                return 'Not!!'
+            }
         }
-
-        return Math.ceil(((res - 1) + 1)/(a*a)) === a ? true : false
+        return "Jumping!!"
     }
 
     console.log(result(arr2))
