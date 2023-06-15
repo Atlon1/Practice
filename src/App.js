198b7587ae1f992401cdc5561189ec1d2047ab29
Atlon1 <maciej.szajstek@gmail.com>
2023-06-15 21:08:58 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = 32
+    const arr2 = 10.1289767789
     // const arr3 = 6
     // const arr4 = 3
     // const arr5 = true
 
 
     const result = (a) => {
-       const res = a.toString().split('')
-        for(let i = 0; i < res.length-1;i++){
-            if (Math.abs(res[i] - res[i + 1] !== 1)){
-                return 'Not!!'
-            }
-        }
-        return "Jumping!!"
+      return +a.toFixed(4).slice(0, -2);
     }
 
     console.log(result(arr2))
