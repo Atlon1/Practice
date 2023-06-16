8f534e38dc8ae4410c70c58214329683e2c55430
Atlon1 <maciej.szajstek@gmail.com>
2023-06-16 22:40:39 +0200

    zadanie z rekurencja

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = 0
+    const arr2 = ["x", "y", ["z"]]
     // const arr3 = '==T'
     // const arr4 = 110
     // const arr5 = true
 
 
     const result = (a) => {
-        if (a === null){
-            return -1
-        }
-        return Number((a / 1.15).toFixed(2))
-
+        let counter = a.length;
+        a.forEach(item => {
+            counter += Array.isArray(item) ? result(item) : 0;
+        });
+        return counter;
     }
 
     console.log(result(arr2))
