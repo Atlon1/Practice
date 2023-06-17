65dc301cb13cf51b6f00edbeeb2910c2e3d46425
Atlon1 <maciej.szajstek@gmail.com>
2023-06-17 12:14:48 +0200

    zadanie z silnia liczb pierwszych

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = [16,17,4,3,5,2]
+    const arr2 = 3
     // const arr3 = '==T'
     // const arr4 = 110
     // const arr5 = true
 
 
     const result = (a) => {
-        return a.map((elem) => a.reduce((a,b) => a * b)/ elem)
+        const primeNumber = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+        let res = 1
+        for (let i = 0; i < a; i++){
+           res = res * primeNumber[i]
+        }
+
+        return res
     }
 
     console.log(result(arr2))
