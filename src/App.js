2b78be8cafbed7e5e9a5f4aff9096e87acfc799c
Atlon1 <maciej.szajstek@gmail.com>
2023-06-17 12:46:02 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -6,25 +6,21 @@ import React, {useState} from "react";
 function App() {
 
 
-    const arr2 = [1, 6, 9, -3, 4, -78, 0]
-    const arr3 = -3
-    const arr4 = 4
+    const arr2 = [2, 3, 4, 5]
+    // const arr3 = -3
+    // const arr4 = 4
     // const arr5 = true
 
 
 
-    const result = (a,b,c) => {
-       for (let i = 0; i < a.length; i++){
-           if (a[i] === b && a[i + 1] === c ){
-               return true
-           } else if (a[i] === c && a[i +1] === b){
-               return true
-           }
-       }
-       return false
+    const result = (a) => {
+      if (a === null){
+          return 0
+      }
+      return a.filter((elem, idx) => idx % 2 === 0).reduce((a,b) => a + b)
     }
 
-    console.log(result(arr2,arr3,arr4))
+    console.log(result(arr2))
 
     return (
         <div>
