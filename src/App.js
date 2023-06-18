f1602de92e5606f377f0e48ae2120a9f5bdc43e1
Atlon1 <maciej.szajstek@gmail.com>
2023-06-18 18:56:46 +0200

    wyszukuje barkujaca liczbe w ciagu

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import React, {useState} from "react";
 
 
-
 function App() {
 
 
-    const arr2 = "xyab"
+    const arr2 = [0, 5, 1, 3, 2, 9, 7, 6, 4]
     // const arr3 = "xzca"
     // const arr4 = 4
     // const arr5 = true
 
 
-
     const result = (a) => {
-        return a.trim() === a && !a.includes("  ")
+        for (let i = 0; i < 10; i++) {
+            if (a.indexOf(i) === -1) {
+                return i
+            }
+        }
     }
 
     console.log(result(arr2))
