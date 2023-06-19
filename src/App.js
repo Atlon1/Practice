030e493009af4d468e20d4f32f62f74030934d91
Atlon1 <maciej.szajstek@gmail.com>
2023-06-19 21:32:23 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import React from "react";
 function App() {
 
 
-    const arr2 = 1500
-    const arr3 = 5
-    const arr4 = 100
-    const arr5 = 5000
+    const arr2 = "!!!??"
+    // const arr3 = 5
+    // const arr4 = 100
+    // const arr5 = 5000
 
 
-    const result = (a, b, c, d) => {
-
-        let years = 0
-        while (a < d) {
-            a = Math.floor((1 +b/100)* a +c)
-            years++
-        }
-
-        return years
+    const result = (a) => {
+        const res = [...a].filter(elem => elem === "?" ).length
+        const res2 = [...a].filter(elem => elem === "!" ).length
+        return res * res2
     }
 
-    console.log(result(arr2, arr3, arr4, arr5))
+    console.log(result(arr2))
 
     return (
         <div>
