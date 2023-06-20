627d58f9ee059a37cc64e39d226d503dfe55aaad
Atlon1 <maciej.szajstek@gmail.com>
2023-06-20 21:45:47 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,26 +5,20 @@ import React from "react";
 function App() {
 
 
-    const arr2 = [8,9,10,4,2]
-    const arr3 = 23
-    // const arr4 = 100
+    const arr2 = -3
+    const arr3 = "/"
+    const arr4 = 0
     // const arr5 = 5000
 
 
-    const result = (a,b) => {
-       const sort = a.sort((a,b) => a - b)
-        const res2 = sort[0] + sort[1]
-        let res = 0
-
-        for (let i = 2; i < a.length; i++){
-            if (res2 <= b){
-              res += 1
-            }
-        }
-        return res
+    const result = (a, b, c) => {
+        return b === "+" ? a + c :
+            b === "-" ? a - c :
+                b === "*" ? a * c :
+                    b === "/" ? a / c : null
     }
 
-    console.log(result(arr2,arr3))
+    console.log(result(arr2, arr3, arr4))
 
     return (
         <div>
