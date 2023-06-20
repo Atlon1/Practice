e52b2fa5772d10a57e92d1195f3214ccfe576208
Atlon1 <maciej.szajstek@gmail.com>
2023-06-20 21:33:21 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import React from "react";
 function App() {
 
 
-    const arr2 = "drake"
-    // const arr3 = 5
+    const arr2 = [8,9,10,4,2]
+    const arr3 = 23
     // const arr4 = 100
     // const arr5 = 5000
 
 
-    const result = (a) => {
-       return a.replace(/[aeiou]/g, "")
+    const result = (a,b) => {
+       const sort = a.sort((a,b) => a - b)
+        const res2 = sort[0] + sort[1]
+        let res = 0
+
+        for (let i = 2; i < a.length; i++){
+            if (res2 <= b){
+              res += 1
+            }
+        }
+        return res
     }
 
-    console.log(result(arr2))
+    console.log(result(arr2,arr3))
 
     return (
         <div>
