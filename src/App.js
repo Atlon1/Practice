cd65ca23c600babb3404cd82ef64af22122cdada
Atlon1 <maciej.szajstek@gmail.com>
2023-06-20 21:12:16 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import React from "react";
 function App() {
 
 
-    const arr2 = "!!!??"
+    const arr2 = "drake"
     // const arr3 = 5
     // const arr4 = 100
     // const arr5 = 5000
 
 
     const result = (a) => {
-        const res = [...a].filter(elem => elem === "?" ).length
-        const res2 = [...a].filter(elem => elem === "!" ).length
-        return res * res2
+       return a.replace(/[aeiou]/g, "")
     }
 
     console.log(result(arr2))
