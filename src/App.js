bfa909e8c3e31e4504770e28968115506ef76d96
Atlon1 <maciej.szajstek@gmail.com>
2023-06-23 20:46:53 +0200

    zadanie z dodaniem znaku - do 3 liczby i nastepnej liczby nieparzystej

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import React from "react";
 function App() {
 
 
-    const arr2 = "Yes?"
+    const arr2 = 454793
     // const arr3 = "/"
     // const arr4 = 0
     // const arr5 = 5000
 
 
     const result = (a) => {
-     return a.includes("?") ? a : `${a}?`
+        a = a.toString().split('')
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] % 2 > 0 && a[i + 1] % 2 > 0) {
+                a[i] = a[i] + "-"
+            }
+        }
+        return a.join('')
     }
 
     console.log(result(arr2))
