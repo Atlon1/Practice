6dc2ef8b8c6a29cba51b311ddbfc68f0e320733c
Atlon1 <maciej.szajstek@gmail.com>
2023-06-24 20:08:43 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,30 +5,33 @@ import React,{useState}from "react";
 function App() {
 
 
-    const arr2 = 'Lois Mary Lane'
+    const arr2 = "aeiou, abc"
     // const arr3 = "/"
     // const arr4 = 0
     // const arr5 = 5000
 
-    const [onClick, setOnClick] = useState(false)
 
-    // const result = (a) => {
-    //
-    // }
 
-    // console.log(result(arr2))
+    const result = (a) => {
+        let res = a.split('')
+        let res2 =[]
+        for (let i = 0; i < res.length; i++){
+            if (res[i] === "a" || res[i] === "e" ||res[i] === "i" ||res[i] === "o" ||res[i] === "u"){
+                res2.push('1')
+            } else {
+                res2.push('0')
+            }
+        }
+
+        return (res2.join('')).toString()
+    }
+
+    console.log(result(arr2))
 
 
     return (
         <div>
-            <div style={
-                {
-                    width: "100px",
-                    height: "100px",
-                    background: onClick ? "black" : "yellow"
-                }
-            }> </div>
-            <button onClick={(e =>{setOnClick(!onClick)})}>On</button>
+
         </div>
     );
 }
