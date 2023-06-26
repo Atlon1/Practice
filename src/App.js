a16dd4b82a5de6d0a2bf569237f28b5c91930b38
Atlon1 <maciej.szajstek@gmail.com>
2023-06-26 20:42:00 +0200

    zadanie z laczenie 2 stringow przy 3 zmiennej ktora jest wyznaczyniekiem

--- src/App.js
+++ src/App.js
@@ -4,83 +4,25 @@ import React, {useEffect, useState} from "react";
 
 function App() {
 
-    const [light, setLight] = useState(0)
-    // const arr2 = "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
-    // const arr3 = "/"
-    // const arr4 = 0
+
+    const arr2 ="hello"
+    const arr3 = "world"
+    const arr4 = "l"
     // const arr5 = 5000
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            switch (light) {
-                case 1:
-                    setLight(2);
-                    break;
-                case 0:
-                    setLight(1);
-                    break;
-                case 2:
-                    setLight(0);
-                    break;
-                default:
-                    break;
-            }
-        }, 2000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [light]);
 
 
-    // const result = (a) => {
-    //     return a.replace(/[aeiou]/g, v => v.toUpperCase())
-    // }
-    //
-    // console.log(result(arr2))
+
+    const result = (a,b,c) => {
+       return `${a.split(c, 1)}${b.slice(b.indexOf(c))}`
+    }
+
+    console.log(result(arr2,arr3,arr4))
 
 
     return (
-        <div style={
-            {
-                background: "grey",
-                width: "125px",
-                height: "400px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: '25px'
-            }
-        }>
-            <div style={
-                {
-                    background: light === 0 ? "green" : "darkgrey",
-                    width: "100px",
-                    height: "100px",
-                    borderRadius: "100%"
-                }
-            }>
-            </div>
-            <div style={
-                {
-                    background: light === 1 ? "orange" : "darkgrey",
-                    width: "100px",
-                    height: "100px",
-                    borderRadius: "100%"
-                }
-            }>
-            </div>
-            <div style={
-                {
-                    background: light === 2 ? "red" : "darkgrey",
-                    width: "100px",
-                    height: "100px",
-                    borderRadius: "100%"
-                }
-            }>
-            </div>
-        </div>
+       <>
+       </>
     );
 }
 
