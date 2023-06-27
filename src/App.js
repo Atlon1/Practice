e42e95bddac20cfbba9ce2cacd9a15ed667dc5e0
Atlon1 <maciej.szajstek@gmail.com>
2023-06-27 20:12:13 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =4
-    const arr3 = 2
+    const arr2 =9007199254740990
+    // const arr3 = 2
     // const arr4 = "l"
     // const arr5 = 5000
 
 
 
 
-    const result = (a,b) => {
-        return Math.pow(a, 1/b)
+    const result = (a) => {
+        return Number.isSafeInteger(a)
 
     }
 
-    console.log(result(arr2,arr3))
+    console.log(result(arr2))
 
 
     return (
