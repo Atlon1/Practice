5a11a7ee155e1d4217b90db95eaafc4672f8174f
Atlon1 <maciej.szajstek@gmail.com>
2023-06-27 20:09:57 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,25 +5,20 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =[7,1,2,3]
-    // const arr3 = "world"
+    const arr2 =4
+    const arr3 = 2
     // const arr4 = "l"
     // const arr5 = 5000
 
 
 
 
-    const result = (f) => {
-        return (
-            !(f.includes(1) && f.includes(2)) &&
-            !(f.includes(3) && f.includes(4)) &&
-            (f.includes(5) === f.includes(6)) &&
-            (f.includes(7) || f.includes(8))
-        )
+    const result = (a,b) => {
+        return Math.pow(a, 1/b)
 
     }
 
-    console.log(result(arr2))
+    console.log(result(arr2,arr3))
 
 
     return (
