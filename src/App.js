30613b39e670b3f373bb2e29efad494506291b3a
Atlon1 <maciej.szajstek@gmail.com>
2023-06-27 20:19:39 +0200

    zadanie z wyplaczersniem obietku

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 = 7
+    const arr2 = [[[1,2,3]]]
     // const arr3 = 2
     // const arr4 = "l"
     // const arr5 = 5000
@@ -14,12 +14,7 @@ function App() {
 
 
     const result = (a) => {
-        let array = [];
-
-        for (let i = 1; i <= a; i += 2) {
-            array.push(i)
-        }
-        return array;
+        return [].concat(...a)
 
     }
 
