27e709029e7b4a833e5d073dbd46d26ef81a672c
Atlon1 <maciej.szajstek@gmail.com>
2023-06-28 22:34:11 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 = [[[1,2,3]]]
-    // const arr3 = 2
-    // const arr4 = "l"
+    const arr2 = [6, 9, 3, 4, 3, 82, 11]
+
+    const arr3 = 3
+    // const arr4 = 98
     // const arr5 = 5000
 
 
 
 
-    const result = (a) => {
-        return [].concat(...a)
+    const result = (a,b) => {
+        return a.forEach((v,i) =>{if(v===b){a.push(i)}})
 
     }
 
-    console.log(result(arr2))
+    console.log(result(arr2,arr3))
 
 
     return (
