0bb87b210a19e8e100b11c132d5a4b99a4735ec2
Atlon1 <maciej.szajstek@gmail.com>
2023-06-30 21:13:14 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 = 42
+    const arr2 = [13,10,2,9,5]
 
     const arr3 = "number"
     const arr4 = 5
@@ -15,8 +15,13 @@ function App() {
 
 
     const result = (a,b,c) => {
+        let res =[]
+        const sort = a.sort((a,b) => a - b).reverse()
+        for (let i = 0; i < a.length - 1; i++){
+            res.push(sort[i] - sort[i + 1])
+        }
 
-        return typeof(a) === b
+        return Math.max(...res)
     }
 
     console.log(result(arr2,arr3,arr4))
