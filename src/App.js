5fcc4357d175b30cb5452d3ba7e5db49c3a0e24f
Atlon1 <maciej.szajstek@gmail.com>
2023-07-01 20:11:13 +0200

    zadanie z czasem

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =[[1,2],[3,4],[5,6]]
+    const arr2 ="19:99:99"
 
     const arr3 = "number"
     const arr4 = 5
@@ -15,9 +15,15 @@ function App() {
 
 
     const result = (a,b,c) => {
-
-
-        return [].concat(...a).sort((a,b) => b -a).join(">")
+        const date = new Date()
+        if (a === ""){
+            return a
+        }
+        if (!/^\d{2}\:\d{2}\:\d{2}$/g.test(a)){
+            return null
+        }
+        date.setUTCHours(...a.split(":"))
+        return date.toLocaleTimeString('en',{hour12: false})
     }
 
     console.log(result(arr2,arr3,arr4))
