f49cf7ba21d9fedce15ca45f6d22d0d8827bcff3
Atlon1 <maciej.szajstek@gmail.com>
2023-07-11 22:27:30 +0200

    task ryd

--- src/App.js
+++ src/App.js
@@ -5,25 +5,23 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 ="Sam"
+    const arr2 =10
 
-    const arr3 = "number"
-    const arr4 = 5
+    const arr3 = 10
+    const arr4 = 21
     // const arr5 = 5000
 
 
 
 
     const result = (a,b,c) => {
-        const res = [...a]
 
-       if (a.length <= 3){
-           return "Error: Name too short"
-       } else if (res[2] === "a" || res[2] === "e" || res[2] === "i" || res[2] === "o" || res[2] === "u"){
-           return a.slice(0,4)
-       } else {
-           return a.slice(0, 3)
-       }
+        if (c > 2 * b) return "Too much clothes";
+        if (c < b) return "Not enough clothes";
+        const res = +(a * 1.1 ** (c - b)).toFixed(2)
+
+
+        return res
 
     }
 
