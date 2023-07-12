bb280ed3484c2962bc5830f0694aedcfd7e4321b
Atlon1 <maciej.szajstek@gmail.com>
2023-07-12 20:44:51 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =["Banana", "Orange", "Apple", "Mango", 0, 2, 2]
+    const arr2 =[5,2,8]
 
     const arr3 = 10
     const arr4 = 21
@@ -15,11 +15,7 @@ function App() {
 
 
     const result = (a,b,c) => {
-
-            return a.reduce(function(x, fn) {
-                return fn(b)
-            }, b)
-
+        return a.slice(1).reduce((s,n,i)=>s+Math.abs(n-a[i]),0)
     }
 
     console.log(result(arr2,arr3,arr4))
