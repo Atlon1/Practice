fa8fe628c348bb09e74fcbae047e7503835f5a75
Atlon1 <maciej.szajstek@gmail.com>
2023-07-13 20:51:46 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =[5,2,8]
+    const arr2 ="a **&  bZ"
 
     const arr3 = 10
     const arr4 = 21
@@ -15,7 +15,8 @@ function App() {
 
 
     const result = (a,b,c) => {
-        return a.slice(1).reduce((s,n,i)=>s+Math.abs(n-a[i]),0)
+        a = a.toLowerCase()
+        return 'abcdefghijklmnopqrstuvwxyz'.split('').map((elem) =>  a.indexOf(elem) !== -1 ? 1 : 0).join('')
     }
 
     console.log(result(arr2,arr3,arr4))
