5d519496d3a31d6a1dbe9fe306ad2969f8358e25
Atlon1 <maciej.szajstek@gmail.com>
2023-07-14 20:47:03 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =[{'country' : 'Spain', 'capital' : 'Madrid'}]
+    const arr2 =3
 
-    const arr3 = 10
-    const arr4 = 21
+    const arr3 = 4
+    const arr4 = 5
     // const arr5 = 5000
 
 
 
 
     const result = (a,b,c) => {
-         return a.map((elem) => `The capital of ${elem.state || elem.country} is ${elem.capital}`)
+        let s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
     console.log(result(arr2,arr3,arr4))
