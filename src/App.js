8f038063149429f7efcaf68f079a72aee0dd7341
Atlon1 <maciej.szajstek@gmail.com>
2023-07-16 21:35:41 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 = [21,13,2,7,5,14,7,15,9,10]
+    const arr2 = "Hello, World!"
 
     const arr3 = 4
     const arr4 = 5
@@ -15,7 +15,7 @@ function App() {
 
 
     const result = (a,b,c) => {
-        return [2,9,14,7,15].every(x => a.includes(x)) ? "WIN" : "LOSE"
+        return a.split(' ').length
     }
 
     console.log(result(arr2,arr3,arr4))
