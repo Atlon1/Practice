5624fedd0bab945664fde76429e0c75e6df72527
Atlon1 <maciej.szajstek@gmail.com>
2023-07-17 21:17:05 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 = "Hello, World!"
+    const arr2 =[1, 2, 3, 4]
 
     const arr3 = 4
     const arr4 = 5
@@ -15,9 +15,8 @@ function App() {
 
 
     const result = (a,b,c) => {
-        a = a.toUpperCase()
-
-        return [...a].map((_, i) => a.slice(i) + a.slice(0, i))
+        let lest = arguments[arguments.length - 1]
+       return a[a.length - 1] || lest
     }
 
     console.log(result(arr2,arr3,arr4))
