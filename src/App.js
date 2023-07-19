dd4632e4ff3f2edd89925f9b57b8d0770a8e6bbb
Atlon1 <maciej.szajstek@gmail.com>
2023-07-19 20:50:11 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 ="This is a string"
+    const arr2 ='1'
 
     const arr3 = 3
     const arr4 = 5
@@ -15,7 +15,13 @@ function App() {
 
 
     const result = (a,b,c) => {
-       return (a.split(' ')[b]+"-").repeat(c).slice(0,-1)
+        if (a <= 1){
+            return null
+        }
+       let even = a.split('').filter((elem) => elem % 2 === 0).map((elem) => parseInt(elem)).reduce((a,b) => a + b)
+       let odd = a.split('').filter((elem) => elem % 2 !== 0).map((elem) => parseInt(elem)).reduce((a,b) => a + b)
+
+        return even > odd ? 'Even is greater than Odd' : even === odd ? 'Even and Odd are the same' : 'Odd is greater than Even'
     }
 
     console.log(result(arr2,arr3,arr4))
