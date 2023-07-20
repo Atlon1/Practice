4e0d097eae01c4758c37448e4a6b7978d7c24dce
Atlon1 <maciej.szajstek@gmail.com>
2023-07-20 21:46:40 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =68
+    const arr2 ="abraxxxas"
 
     const arr3 = 3
     const arr4 = 5
@@ -15,7 +15,7 @@ function App() {
 
 
     const result = (a,b,c) => {
-        return a > 0 ? +(a * a * Math.PI).toFixed(2) : false
+        return /^[^x]*xxx/.test(a)
     }
 
     console.log(result(arr2,arr3,arr4))
