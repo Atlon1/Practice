7eee7f1ea5d516adbbde3d369fe27a0a2367f68d
Atlon1 <maciej.szajstek@gmail.com>
2023-07-22 12:42:57 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 =[1,2,3]
+    const arr2 ="abd"
 
     const arr3 = 10
     const arr4 = 5
@@ -15,7 +15,7 @@ function App() {
 
 
     const result = (a,b,c) => {
-        return a.reverse()
+        return 'abcdefghijklmnopqrstuvwxyz'.includes([...a].sort().join(''))
 
 
     }
