6daee7bb0d7a22e43a81072dd1d75d553c325ad5
Atlon1 <maciej.szajstek@gmail.com>
2023-07-24 14:03:32 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import React, {useEffect, useState} from "react";
 function App() {
 
 
-    const arr2 ='a'
+    const arr2 =1892376
 
-    const arr3 = 10
+    const arr3 = 5
     const arr4 = 5
     // const arr5 = 5000
 
@@ -15,8 +15,9 @@ function App() {
 
 
     const result = (a,b,c) => {
-       return /^[aeiou]$/i.test(a)
+        const res = a.toString().split('').map((elem) => parseInt(elem)).reduce((a,b) => a + b)
 
+        return res % 9 === 0
     }
 
     console.log(result(arr2,arr3,arr4))
