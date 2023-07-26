a324b7e7a9f6ff005e77a6cef954e65770b5974d
Atlon1 <maciej.szajstek@gmail.com>
2023-07-26 23:04:38 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr2 = [12, 34, 56, 78]
+    const arr2 = [49,3,5,300,7]
     const arr3 = 5
     const arr4 = 1.8
     // const arr5 = 5000
@@ -13,15 +13,13 @@ function App() {
 
 
 
-    const result = (...a) => {
-        const numbers = [0,1,2,3,4,5,6,7,8,9]
+    const result = (a) => {
+       let count = Math.round(a.reduce((a,b) => a + b)/a.length)
 
-        let  numStr = a.join('')
-
-        return numbers.filter(elem => !numStr.includes(elem)).join('')
+        return count
     }
 
-    console.log(result(arr2,arr3,arr4))
+    console.log(result(arr2))
 
 
     return (
