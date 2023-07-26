7c08d8e2dae500dc160b641207b9a6e8f9216537
Atlon1 <maciej.szajstek@gmail.com>
2023-07-26 23:00:42 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr2 = [2, 8, 10, 9, 1, 3, 4, 7, 6, 5]
+    const arr2 = [12, 34, 56, 78]
     const arr3 = 5
     const arr4 = 1.8
     // const arr5 = 5000
@@ -13,8 +13,12 @@ function App() {
 
 
 
-    const result = (a,b,c) => {
-     return a.sort((a,b) => a - b).reverse().map((elem)=> elem.toString()).join(" ") + " liftoff!"
+    const result = (...a) => {
+        const numbers = [0,1,2,3,4,5,6,7,8,9]
+
+        let  numStr = a.join('')
+
+        return numbers.filter(elem => !numStr.includes(elem)).join('')
     }
 
     console.log(result(arr2,arr3,arr4))
