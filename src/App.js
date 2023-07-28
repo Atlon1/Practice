2d06d11023d7c68bd94098df9367beadce4c9e47
Atlon1 <maciej.szajstek@gmail.com>
2023-07-28 20:16:28 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,21 +5,16 @@ import React from "react";
 function App() {
 
 
-    const arr2 = 1234
-    const arr3 = 5
-    const arr4 = 1.8
+    const arr2 = 3
+    const arr3 = 3
+    const arr4 = 4
     // const arr5 = 5000
 
 
 
 
-    const result = (a) => {
-        let res = 0;
-        while (a) {
-            res = res * 10 + a % 10;
-            a = a / 10 ^ 0;
-        }
-        return res;
+    const result = (a, ...b) => {
+        return b.every(n => a % n === 0);
     }
 
     console.log(result(arr2))
