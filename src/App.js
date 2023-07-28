57d2ae4024ca72f789318152cde7e24b1218a932
Atlon1 <maciej.szajstek@gmail.com>
2023-07-28 20:21:31 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr2 = 3
+    const arr2 = ['kevin', 'has','no','space']
     const arr3 = 3
     const arr4 = 4
     // const arr5 = 5000
@@ -13,8 +13,15 @@ function App() {
 
 
 
-    const result = (a, ...b) => {
-        return b.every(n => a % n === 0);
+    const result = (a) => {
+        let res =[]
+        let res2 =[]
+        for (let i = 0; i < a.length; i++){
+            res += a[i]
+            res2.push(res)
+        }
+
+        return res2
     }
 
     console.log(result(arr2))
