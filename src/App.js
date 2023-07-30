324aa16b2f3022ac25b37268644b8315c5fc7647
Atlon1 <maciej.szajstek@gmail.com>
2023-07-30 20:54:14 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
 
 
-    const arr1 = 15
+    const arr1 = [1,21,4,7,5]
     const arr2 = 15
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -14,10 +14,7 @@ function App() {
 
 
     const result = (a,b) => {
-       if (a > 0 && b > 0) return 1
-       if (a < 0 && b > 0) return 2
-       if (a < 0 && b < 0) return 3
-        return 4
+       return a.filter((elem, idx)=>a.slice(idx+1).every(z=> z < elem) )
     }
 
     console.log(result(arr1,arr2))
