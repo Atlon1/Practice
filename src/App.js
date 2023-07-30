593fa42349b5e48bddbe93bfa2dba6102f3f9f08
Atlon1 <maciej.szajstek@gmail.com>
2023-07-30 20:38:26 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'ghdfj32'
-    // const arr2 = [1, 2, 5]
+    const arr1 = 15
+    const arr2 = 15
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (string) => {
- return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{6,}$/.test(string)
+    const result = (a,b) => {
+        return [(a < 24) ? a /15 : (a -16) / 4,
+            (b< 24) ? b/15 : (b-16) / 5].map(Math.floor)
 
     }
 
 
 
-    console.log(result(arr1))
+    console.log(result(arr1,arr2))
 
 
     return (
