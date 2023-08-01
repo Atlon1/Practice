68b7e9151d3b5a63e04216d774ae0973cc05cda4
Atlon1 <maciej.szajstek@gmail.com>
2023-08-01 20:15:39 +0200

    task redy

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
 
 
 
-    const arr1 = 1024
-    const arr2 = "4:3"
+    const arr1 = 6
+    const arr2 = 3
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a,b) => {
-       const [ab, bc ] = b.split(":")
-        const height = a / ab * bc
-        return a + "x" + height
+       return a % b ? -1 : [b, a-b]
     }
 
     console.log(result(arr1,arr2))
