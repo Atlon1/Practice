f6e60d019e024a832b43aba9cbce5b34e1124534
Atlon1 <maciej.szajstek@gmail.com>
2023-08-03 19:24:40 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import React from "react";
 function App() {
 
 
-
-    const arr1 = ["yoda", "best", "has"]
+    const arr1 = 123546
     const arr2 = 3
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-     return a.reduce((a,b,index) => a + b [index], "")
+    const result = (n,d) => {
+        const nums = String(n).split('').map(Number);
+        const len = nums.length;
+        let index = 0;
+        return nums.filter(el => index++ >= len - d);
     }
 
     console.log(result(arr1,arr2))
