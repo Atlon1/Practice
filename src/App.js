3ea21003a91e39ec56cae608f09a4a4d90349a30
Atlon1 <maciej.szajstek@gmail.com>
2023-08-04 22:35:40 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 4
+    const arr1 = ['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']
     const arr2 = 4
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,7 +13,11 @@ function App() {
 
 
     const result = (a,b) => {
-        return a**b
+       let res = a.filter((elem)=> elem.replace(/[0-9]/g, "")).join('')
+       let res2 = a.filter((elem)=> elem.replace(/[a-z]/g, "")).map((elem)=> parseInt(elem))
+        let res3 = res2.reduce((a,b)=> a+b)
+
+        return [res3/res2.length, res]
     }
 
     console.log(result(arr1,arr2))
