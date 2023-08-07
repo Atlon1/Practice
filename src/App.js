ddb9566c3200f7899b891f62480b1ca5fcbe03be
Atlon1 <maciej.szajstek@gmail.com>
2023-08-07 21:25:24 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'What feck damn cake'
+    const arr1 = [['gOOd', 'bad', 'BAD', 'bad', 'bad'], ['bad', 'bAd', 'bad'], ['GOOD', 'bad', 'bad', 'bAd']]
     const arr2 = 4
     // const arr3 = 0.18
     // const arr4 = 1200
@@ -13,9 +13,8 @@ function App() {
 
 
     const result = (a,b) => {
-        const res = a.split(' ').map((elem)=> elem.toUpperCase() + "!!!!").join(' ')
-
-        return res.replace(/A/g, "@").replace(/[AEIOU]/g, "*")
+        const res = a.toString().toLowerCase().split(/good/g).length-1
+        return res > 2 ? "I smell a series!" : res >0 ? "Publish" : "Fail"
     }
 
     console.log(result(arr1,arr2))
