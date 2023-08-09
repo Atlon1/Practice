2cbde8333e51f354ea2da5df0261b4dfbc99c6df
Atlon1 <maciej.szajstek@gmail.com>
2023-08-09 22:48:02 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 'alaska'
-    const arr2 = 4
+    const arr1 = "WXGG klOL"
+    const arr2 = 2
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a,b) => {
-       const res = a.split('')
-       let res2 = res[0].toUpperCase()
-        return res[0] === res[res.length-1] ? `${res2 + res.slice(1).join('')+ res.slice(1).join('')}` : `The ${res2 + res.slice(1).join('')}`
+       return a.length <= b ? `${a}` : a.length <= 4 ? `${a.slice(0,(b - a.length ))}...` :`${a.slice(0,(b - a.length - 3 ))}...`
 
     }
 
