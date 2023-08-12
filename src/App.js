06b61610bcc331913116be10b6277feac89719cb
Atlon1 <maciej.szajstek@gmail.com>
2023-08-12 21:55:06 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ import React from "react";
 function App() {
 
 
-    const arr1 = []
-    const arr2 = 'bb'
+    const arr1 = ['a', 'b', 'c', 'd', 'e']
+    const arr2 = [1, 2, 3, 4, 5]
     // const arr3 = 0.18
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b) => {
-        if (a == null) return 0;
-        return 1 + (a.next);
+    const result = (a, b) => {
+        let res = []
+        if (a.length > b.length) {
+            for (let i = 0; i < a.length; i++) {
+               if(a[i])  res.push(a[i])
+                if(b[i]) res.push(b[i])
+            }
+        } else {
+            for (let i = 0; i < b.length; i++) {
+                if(a[i])  res.push(a[i])
+                if(b[i]) res.push(b[i])
+            }
+        }
+        return res
     }
-
-    console.log(result(arr1,arr2))
-
+    console.log(result(arr1, arr2))
 
 
     return (
-       <>
-       </>
+        <>
+        </>
     );
 }
 
