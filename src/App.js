7ce720f328036b4809cc9c84f54617119016290b
Atlon1 <maciej.szajstek@gmail.com>
2023-08-16 20:45:44 +0200

    zadanie z dekodowaniem wiadomosci

--- src/App.js
+++ src/App.js
@@ -5,21 +5,16 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 3
-    const arr2 = 4
+    const arr1 = [ 20, 12, 18, 30, 21]
+    const arr2 = 1939
     const arr3 = 5
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a,b,c) => {
-        const arr = []
-        for (let i = a; i < c; i++){
-            if( i % a === 0 && i % b === 0 ){
-                arr.push(i)
-            }
-        }
-        return arr
+        const key = String(b)
+        return String.fromCharCode(...a.map((c, i) => c - Number(key[i % key.length]) + 96))
     }
     console.log(result(arr1, arr2, arr3))
 
