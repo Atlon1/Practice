147a36a8c9eb3b197bd546f040992e979ac131ae
Atlon1 <maciej.szajstek@gmail.com>
2023-08-19 21:48:46 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [ 20, 12, 18, 30, 21]
+    const arr1 = "door"
     const arr2 = 1939
     const arr3 = 5
     // const arr4 = 1200
@@ -13,8 +13,7 @@ function App() {
 
 
     const result = (a,b,c) => {
-        const key = String(b)
-        return String.fromCharCode(...a.map((c, i) => c - Number(key[i % key.length]) + 96))
+        return a === a.split('').sort().join('')
     }
     console.log(result(arr1, arr2, arr3))
 
