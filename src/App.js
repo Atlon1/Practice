cd90aa5793fd3105de29073c76c7a42bb4a51060
Atlon1 <maciej.szajstek@gmail.com>
2023-08-19 21:56:33 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import React from "react";
 function App() {
 
 
-    const arr1 = "door"
-    const arr2 = 1939
-    const arr3 = 5
+    const arr1 = 2
+    const arr2 = 10
+    const arr3 = 3
     // const arr4 = 1200
     // const arr5 = true
 
 
-    const result = (a,b,c) => {
-        return a === a.split('').sort().join('')
+    const result = (a, b, c) => {
+        return [...arguments].filter(elem => elem > 0).length === 2
     }
     console.log(result(arr1, arr2, arr3))
 
