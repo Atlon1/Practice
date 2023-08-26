4594e93635133797981c3b41f0a71802eae39495
Atlon1 <maciej.szajstek@gmail.com>
2023-08-26 21:07:41 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ function App() {
 
     const arr1 = 88
     const arr2 = 4
-    // const arr3 = 0.18
+    // const arr3 = 0.18ą
     // const arr4 = 1200
     // const arr5 = true
 
 
     const result = (a,b) => {
-        return a.length ? Math.max(...a.map(x=>x.filter(y=>y==x).length)) : 0
+
     }
 
     console.log(result(arr1,arr2))
-
-
 
     return (
        <>
