5f1a50e04ce1f12785d000c75e54dd17aab44717
Atlon1 <maciej.szajstek@gmail.com>
2023-09-03 17:41:09 +0200

    generator gotowy

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 
 
 function App() {
 
 
-
-    const arr1 = 10
+    const arr1 = 100
     const arr2 = 3
     const arr3 = 1
     // const arr4 = 1200
     // const arr5 = true
 
 
-
     const result = (a, b, c) => {
         let res = []
         let min = 1
         let max = 49
-        for (let i = 0; i < a; i++){
-            const row =[]
-            for (let j = 0; j < 6; j++){
+        for (let i = 0; i < a; i++) {
+            const row = []
+            for (let j = 0; j < 6; j++) {
                 let num = min + Math.floor((max - min) * Math.random())
                 row.push(num)
             }
             res.push(row)
         }
+        return res.filter((item, index, arr) => arr.indexOf(item) === index)
 
-        return res
     }
 
+    const [generateNumbers, setGenerateNumbers] =useState([])
 
-    console.log(result(arr1,arr2, arr3))
 
-
+    const genertateButton = () =>{
+        const number = result(arr1)
+        setGenerateNumbers(number)
+    }
     return (
-        <>
-        </>
+     <div>
+         <button onClick={genertateButton}>Wygeneruj Liste</button>
+         {generateNumbers.length > 0 && (
+             <ul>
+                 {generateNumbers.map((elem, index) => (
+                     <li key={index}>
+                         {elem.join(', ')}
+                     </li>
+                 ))}
+             </ul>
+         )}
+     </div>
     );
 }
 
