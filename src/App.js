faef9fcf97b97cc8b427cc84f8195af2c4299080
Atlon1 <maciej.szajstek@gmail.com>
2023-09-11 20:45:31 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React from "react";
 function App() {
 
 
-    const arr1 = 5
+    const arr1 = 10
     // const arr2 = 'Phoenix'
     // const arr3 = 'Arizona'
     // const arr4 = 8
@@ -14,18 +14,10 @@ function App() {
 
 
     const result = (a) => {
-        let res = ''
-        for (let i = 1; i <= a; i++){
-            for (let j = 1; j <= a; j++){
-                if (j < a){
-                    let res2 = j * i
-                    res += ` ${j} * ${i} = ${res2} |`
-                } else {
-                    let res2 = j * i
-                    res += ` ${j} * ${i} = ${res2}`
-                }
-            }
-            res += `\n`
+        let res = ""
+        for (let i = 0; i <= a; i++){
+            res += "*".repeat(i)
+            res += " ".repeat(a - i) +  "\n"
         }
         return res
     }
