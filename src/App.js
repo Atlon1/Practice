aa3c391cbe2d5f05ae3b8251cf45c7492c91edc5
Atlon1 <maciej.szajstek@gmail.com>
2023-09-23 00:21:09 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ import React from "react";
 function App() {
 
 
-    const arr1 = ["google", "twitter", "facebook"]
-    // const arr2 = 5
+    const arr1 = {
+        name: "Mruczek",
+        age: 10,
+        getVoice: () => "miau miau"
+    };
+    const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil" ]
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
 
 
+
+
+
     const result = (a) => {
+
+
     }
 
+    const {name, age, getVoice} = arr1
 
-    console.log(result(arr1))
+    console.log((`Kot ${name} ma ${age} lat i robi ${getVoice()}`))
 
 
     return (
         <>
-            {arr1.map((elem, index)=> {
-                return (
-                    <li key={index}><a href={`https://${elem}.com`}>{`https://${elem}.com`}</a> </li>
-                )
-            })}
+
         </>
     );
 }
