a9b0a897a9fb765fa1f398034499096c698e7546
Atlon1 <maciej.szajstek@gmail.com>
2023-09-23 11:31:19 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ import React from "react";
 function App() {
 
 
-    const arr1 = {
-        name: "Mruczek",
-        age: 10,
-        getVoice: () => "miau miau"
-    };
+    const arr1 = 123
     const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil" ]
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
 
-
+    const numA = parseInt(prompt('Numer z a'))
+    const numB = parseInt(prompt('Numer z B'))
+   const res = numA + numB
 
 
 
@@ -25,14 +23,12 @@ function App() {
 
     }
 
-    const {name, age, getVoice} = arr1
 
-    console.log((`Kot ${name} ma ${age} lat i robi ${getVoice()}`))
 
 
     return (
         <>
-
+            {res}
         </>
     );
 }
