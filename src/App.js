5096a83df912c14e145a0b4d08700b281048aa4c
Atlon1 <maciej.szajstek@gmail.com>
2023-09-24 21:38:20 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import './App.css';
 import React from "react";
 
 
-
 function App() {
 
 
-    const arr1 =  [{
+    const arr1 = [{
         title: "Mr.",
         firstName: "John",
         lastName: "Doe",
         age: 41
     }];
-    const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil" ]
+    const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil"]
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
 
-
-
+    const numA = parseInt(prompt("Cyfra 1"))
+    const numB = parseInt(prompt("Cyfra 2"))
+    const res = prompt("Działanie")
 
     const result = (a) => {
 
@@ -26,20 +26,13 @@ function App() {
     }
 
 
-
-
     return (
         <>
-            {arr1.map((elem)=> {
-                return (
-                    <ul>
-                        <li>{elem.firstName}</li>
-                        <li>{elem.age}</li>
-                        <li>{elem.lastName}</li>
-                        <li>{elem.title}</li>
-                    </ul>
-                )
-            })}
+            <h1>
+                {res === "*" ? numA * numB :
+                    res === "/" ? numA / numB :
+                        res === "+" ? numA + numB : numA - numB}
+            </h1>
         </>
     );
 }
