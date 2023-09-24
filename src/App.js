16a485ed8b7d20c2fcdd2ef64fe6dd996af9a9b7
Atlon1 <maciej.szajstek@gmail.com>
2023-09-24 21:57:30 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -5,39 +5,65 @@ import React from "react";
 function App() {
 
 
-    const arr1 = [{
-        title: "Mr.",
-        firstName: "John",
-        lastName: "Doe",
-        age: 41
-    }];
+    const arr1 = [
+        {
+            id: 1,
+            title: "Mrs.",
+            name: "Sonya",
+            surname: "Smith",
+            pesel: 66111626126,
+            bio:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla, diam non tempus vulputate, tellus massa viverra diam, eget scelerisque ipsum ipsum vel sem.",
+            avatar: "https://image.flaticon.com/icons/svg/145/145852.svg"
+        },
+        {
+            id: 2,
+            title: "Mr.",
+            name: "Charles",
+            surname: "Willard",
+            pesel: 53052334461,
+            bio:
+                "Nullam in dignissim nibh. Sed lorem arcu, consectetur vel posuere quis, cursus sit amet massa. In eleifend ut urna in sodales. Vestibulum viverra lectus in efficitur ultricies.",
+            avatar: "https://image.flaticon.com/icons/svg/145/145867.svg"
+        },
+        {
+            id: 3,
+            title: "Ms.",
+            name: "Tina",
+            surname: "Robinson",
+            pesel: 67032584953,
+            bio:
+                "Phasellus euismod leo non lacus congue dapibus. Curabitur iaculis faucibus euismod. Vestibulum sem metus, congue at lacinia eget, suscipit at nunc.",
+            avatar: "https://image.flaticon.com/icons/svg/145/145862.svg"
+        }
+    ];
     const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil"]
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
 
-
+    const imageUrl = "https://fer-api.coderslab.pl/assets/pexels-photo-4974914.jpeg";
 
     const result = () => {
 
-        const numA = parseInt(prompt("Cyfra 1"))
-        const numB = parseInt(prompt("Cyfra 2"))
-        const res = prompt("Działanie")
-        return (
-
-            <h1>
-                {res === "*" ? numA * numB :
-                    res === "/" ? numA / numB :
-                        res === "+" ? numA + numB : numA - numB}
-            </h1>
-        )
 
     }
 
 
+
     return (
         <>
-            {result()}
+            {arr1.map((elem)=> {
+                return (
+                    <div className="person">
+                        <img src={ elem.avatar } />
+                        <div className="info">
+                            <h1>{`${elem.name} ${elem.title} ${elem.surname}`}</h1>
+                            <p>{ elem.bio }</p>
+                        </div>
+                    </div>
+                )
+            })}
         </>
     );
 }
