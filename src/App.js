b1f4bbcaf48c027494d6f126efa711bafa2cd6c0
Atlon1 <maciej.szajstek@gmail.com>
2023-09-25 19:45:49 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import './App.css';
 import React from "react";
-import UserDetails from "./UserDetails";
+
 
 
 function App() {
 
 
-    const data =[
-        {
-            id: 1,
-            name: "Rower",
-            price: 999.99
-        },
-        {
-            id: 2,
-            name: "Kask",
-            price: 150.00
-        },
-        {
-            id: 3,
-            name: "Rękawiczki",
-            price: 63.99
-        },
-        {
-            id: 4,
-            name: "Licznik",
-            price: 225.49
-        },
-        {
-            id: 5,
-            name: "Bidon",
-            price: 20.00
-        }
-    ]
+    const data =123
     const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil"]
     // const arr3 = 'Arizona'
     // const arr4 = 8
@@ -49,7 +23,7 @@ console.log(result())
 
     return (
         <>
-            <UserDetails data={data}/>
+
         </>
     );
 }
