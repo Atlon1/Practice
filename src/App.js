c33bdbb85475201131f058ebc7e929f5a533dc6e
Atlon1 <maciej.szajstek@gmail.com>
2023-09-25 21:54:30 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -14,16 +14,29 @@ function App() {
 
 
     const result = () => {
+    }
 
+    const handleFirst = () =>{
+        console.log('pierwszy przycisk')
+    }
 
+    const handleSec = () => {
+        const numA = parseInt(prompt('Podaj liczbe a'))
+        const numB = parseInt(prompt('Podaj liczbe b'))
+        console.log(Math.pow(numA, numB))
+    }
 
+    const handleThird = () => {
+        console.log(window.innerWidth, window.innerHeight)
     }
 
 console.log(result())
 
     return (
         <>
-
+                <button onClick={handleFirst}>klik 1</button>
+                <button onClick={handleSec}>klik 2</button>
+                <button onClick={handleThird}>klik 3</button>
         </>
     );
 }
