0fdc3afdbcb8d7664ec8580c333ba8612abb9242
Atlon1 <maciej.szajstek@gmail.com>
2023-09-26 20:27:20 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import Func from "./Func";
 
 
 
@@ -16,27 +17,13 @@ function App() {
     const result = () => {
     }
 
-    const handleFirst = () =>{
-        console.log('pierwszy przycisk')
-    }
-
-    const handleSec = () => {
-        const numA = parseInt(prompt('Podaj liczbe a'))
-        const numB = parseInt(prompt('Podaj liczbe b'))
-        console.log(Math.pow(numA, numB))
-    }
-
-    const handleThird = () => {
-        console.log(window.innerWidth, window.innerHeight)
-    }
 
 console.log(result())
 
     return (
         <>
-                <button onClick={handleFirst}>klik 1</button>
-                <button onClick={handleSec}>klik 2</button>
-                <button onClick={handleThird}>klik 3</button>
+                <Func name='Maciej'
+                        surname = 'Szajstek'/>
         </>
     );
 }
