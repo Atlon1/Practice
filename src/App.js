756588265f8bdefd52150e3f5b87fc731e7d1949
Atlon1 <maciej.szajstek@gmail.com>
2023-09-29 22:05:56 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -10,31 +10,25 @@ function App() {
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
-    const [box, setBox] = useState([])
+    const [box, setBox] = useState('lolololol')
 
-    const handlebox = () => {
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        setBox(prevState => [...box, randomColor])
-    }
 
-    console.log(box)
+const handleUpper = () => {
+        setBox(prevState => prevState.toUpperCase())
+}
+const handleLower = () => {
+        setBox(prevState => prevState.toLowerCase())
+}
+
+
+
 
     return (
         <>
-            <button onClick={handlebox}>dodaj boxa</button>
-            {box.map((elem, index)=> {
-                return (
-                    <div
-                        key={index}
-                        style={
-                        {
-                            width: '200px',
-                            height: '200px',
-                            backgroundColor: elem
-                        }
-                    }></div>
-                )
-            })}
+       <h1>{box}</h1>
+            <button onClick={handleUpper}>upper</button>
+            <button onClick={handleLower}>lower</button>
+
 
         </>
     );
