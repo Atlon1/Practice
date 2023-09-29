caea0d7d2f27d8df5f6404217b44f8182872ed28
Atlon1 <maciej.szajstek@gmail.com>
2023-09-29 22:00:57 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -10,65 +10,32 @@ function App() {
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
-    const [color, setColor] = useState('Gray')
-    const [number, setNumber] = useState(1)
+    const [box, setBox] = useState([])
 
-    useEffect(()=>{
-        const interval = setInterval(()=> {
-            switch (number){
-                case 1:
-                    setNumber(2)
-                    setColor('red')
-                    break
-                case 2:
-                    setNumber(3)
-                    break
-                case 3:
-                    setNumber(4)
-                    setColor('green')
-                    break
-                case 4:
-                    setNumber(5)
-                    setColor('orange')
-                    break
-                case 5:
-                    setNumber(2)
-                    setColor('red')
-                    break
-                default:
-                    setNumber(6)
-                    setColor('grey')
-            }
-        },3000)
-        return () => clearInterval(interval)
-    },[number])
+    const handlebox = () => {
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        setBox(prevState => [...box, randomColor])
+    }
+
+    console.log(box)
 
     return (
         <>
-            <div style={
-                {
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '50%',
-                    backgroundColor: number === 2 ? color : number === 3 ? 'red' : 'grey'
-                }
-            }></div>
-            <div style={
-                {
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '50%',
-                    backgroundColor: number === 5 ? color : number === 3 ? 'orange' : 'grey'
-                }
-            }></div>
-            <div style={
-                {
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '50%',
-                    backgroundColor: number === 4 ? color : 'grey'
-                }
-            }></div>
+            <button onClick={handlebox}>dodaj boxa</button>
+            {box.map((elem, index)=> {
+                return (
+                    <div
+                        key={index}
+                        style={
+                        {
+                            width: '200px',
+                            height: '200px',
+                            backgroundColor: elem
+                        }
+                    }></div>
+                )
+            })}
+
         </>
     );
 }
