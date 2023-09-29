2ac375b89fc37cad272015d11fc5549184fec686
Atlon1 <maciej.szajstek@gmail.com>
2023-09-29 22:12:41 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ function App() {
 
 
     const data = 123
-    const arr2 = ["Ala", "Puszek", "Zosia", "Marcin", "Kamil"]
+    const arr2 = [54,76,24,63,4,54,82,36,13,80,10,69,4,23,40]
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
-    const [box, setBox] = useState('lolololol')
 
+    const [box, setBox] = useState(arr2)
 
-const handleUpper = () => {
-        setBox(prevState => prevState.toUpperCase())
+const parzy = () => {
+        const filtered = arr2.filter((elem)=> elem % 2 !== 1)
+        setBox(filtered)
 }
-const handleLower = () => {
-        setBox(prevState => prevState.toLowerCase())
+const nieparzy = () => {
+        const filtered = arr2.filter((elem)=> elem % 2 === 1)
+        setBox(filtered)
 }
 
 
@@ -25,10 +27,15 @@ const handleLower = () => {
 
     return (
         <>
-       <h1>{box}</h1>
-            <button onClick={handleUpper}>upper</button>
-            <button onClick={handleLower}>lower</button>
-
+            <button onClick={parzy}>Parzyste</button>
+            <button onClick={nieparzy}>NieParzyste</button>
+            <ul>
+                {box.map((ele)=> {
+                    return (
+                        <li>{ele}</li>
+                    )
+                })}
+            </ul>
 
         </>
     );
