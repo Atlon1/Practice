470533226b687052778bf1ebd96ed05793af3a97
Atlon1 <maciej.szajstek@gmail.com>
2023-10-01 20:52:32 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
-import SecretStuff from "./SecretStuff";
-
+import ShowInfo from "./Examples/ShowInfo";
 
 
 
@@ -9,18 +8,15 @@ function App() {
 
 
     const data = 123
-    const arr2 = [54,76,24,63,4,54,82,36,13,80,10,69,4,23,40]
+    const arr2 = [54, 76, 24, 63, 4, 54, 82, 36, 13, 80, 10, 69, 4, 23, 40]
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
-
-
-
-
+ const [text, setText] = useState('wartosc')
 
     return (
         <>
-            <SecretStuff password = '123' correctPass = '1233' secret = 'secret'/>
+            <ShowInfo text={text}/>
         </>
     );
 }
---
+++ src/Examples/ShowInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ShowInfo = (props) => {
+    return (
+        <div>
+            {props.text}
+        </div>
+    );
+};
+
+export default ShowInfo;
