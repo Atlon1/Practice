39029d72ce8a6357b0ce336c217e287dd182b8de
Atlon1 <maciej.szajstek@gmail.com>
2023-10-03 21:17:15 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
+import ButtonClick from "./ButtonClick";
 
 
 
@@ -11,25 +12,19 @@ function App() {
     // const arr3 = 'Arizona'
     // const arr4 = 8
     // const arr5 = true
-    const [top, setTop] = useState(0)
-    const [left, setLeft] = useState(0)
+const [count, setCount] = useState(0)
 
     const handleClick = () => {
-        setLeft(Math.floor((1000) * Math.random()))
-        setTop(300 + Math.floor((600 - 300) * Math.random()))
+        setCount(count + 1)
     }
 
     return (
         <>
-            <div style={ {
-                width: 100,
-                height: 100,
-                position: 'absolute',
-                backgroundColor: 'red',
-                top: top,
-                left: left,
-            }}
-            onMouseEnter={handleClick}/>
+        <div>
+            <h1>{count}</h1>
+           <ButtonClick onDone={handleClick}/>
+           <ButtonClick onDone={handleClick}/>
+        </div>
         </>
     );
 }
---
+++ src/ButtonClick.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ButtonClick = (oneDone) => {
+
+
+
+
+    return (
+
+        // eslint-disable-next-line no-unused-expressions
+        <button onClick={oneDone.onDone}>Count</button>
+    );
+};
+
+export default ButtonClick;
