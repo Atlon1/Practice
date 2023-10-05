4608f95c16ffcaaa1dc0b9635916f264564d685b
Atlon1 <maciej.szajstek@gmail.com>
2023-10-05 23:46:14 +0200

    task rdy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, {useEffect, useState} from "react";
 
 
-
 function App() {
 
 
@@ -12,27 +11,29 @@ function App() {
     // const arr4 = 8
     // const arr5 = true
 
-const [number, setNumber] = useState(0);
+    const [text, setText] = useState(['','Anna Kowalska', 'Jan Kowalski', 'Maciej von Handerburg', 'Jurand ze Spychowa'])
+    const [value, setValue] = useState([])
 
+    const handleChange = (e) => {
+        setValue([...value, e.target.value])
+        setText(text.filter(item => item !== e.target.value))
+    }
 
     return (
         <>
             <form>
-                <input
-                    onChange={(e) => setNumber(e.target.value)}
-                    type='number'/>
+                <select value={text[0]} onChange={handleChange}>
+                    {text.map((elem) => {
+                        return <option key={elem} value={elem}>{elem}</option>
+                    })}
+                </select>
+
+                <ul>
+                    {value.map((elem) => {
+                        return <li key={elem}>{elem}</li>
+                    })}
+                </ul>
             </form>
-            <ul>
-                {Array.from({ length: number }, (_, i ) => (
-                    <li style={
-                        {
-                            backgroundColor: 'red',
-                            height: '50px',
-                            width: `${i * 50}px`,
-                        }
-                    } key={i}></li>
-                ))}
-            </ul>
         </>
 
     );
